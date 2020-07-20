3ec929ba4151d7a772b1af26fa913720ffedb1db
newoo <newoo4297@naver.com>
2020-07-21 03:33:01 +0900

    Update AboutPage alignment of components
    Arrange components to center

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const SubTitle = styled.h2({
+  textAlign: 'center',
+});
+
+const Description = styled.p({
+  textAlign: 'center',
+});
+
 export default function AboutPage() {
   return (
     <div>
-      <h2>이 서비스에 대해서...</h2>
-      <p>이 서비스는 영국에서 시작되었습니다.</p>
-      <p>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</p>
+      <SubTitle>이 서비스에 대해서...</SubTitle>
+      <Description>이 서비스는 영국에서 시작되었습니다.</Description>
+      <Description>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</Description>
     </div>
   );
 }
