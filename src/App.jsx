3cf3e9d9fd9e801e4e995096386403cf50dbc8fe
jiunkoo <jiunkoo0106@gmail.com>
2020-07-22 22:04:36 +0900

    Emotion 설치 및 테스트 - @emotion/core @emotion/styled 설치 - App, HomePage에 emotion 적용 - MenuList, MenuItem 스타일 컴포넌트 생성 - RegionsContainer, CategoriesContainer에 스타일 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
 
 import { useDispatch } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import LoginPage from './LoginPage';
@@ -19,6 +21,27 @@ import { setAccessToken } from './actions';
 
 import { loadItem } from './services/storage';
 
+const Container = styled.div({
+  margin: '0 auto',
+  width: '90%',
+});
+
+const Header = styled.header({
+  background: '#EEE',
+  '& h1': {
+    fontSize: '1.5em',
+    margin: 0,
+    padding: '1em .5em',
+  },
+  '& a': {
+    color: '#555',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#000',
+    },
+  },
+});
+
 export default function App() {
   const dispatch = useDispatch();
 
@@ -28,12 +51,12 @@ export default function App() {
   }
 
   return (
-    <div>
-      <header>
+    <Container>
+      <Header>
         <h1>
-          <Link to="/">헤더 영역</Link>
+          <Link to="/">Eat Go</Link>
         </h1>
-      </header>
+      </Header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
@@ -42,6 +65,6 @@ export default function App() {
         <Route path="/restaurants/:id" component={RestaurantPage} />
         <Route component={NotFoundPage} />
       </Switch>
-    </div>
+    </Container>
   );
 }
