02ddad12a453db110d7917d3ccb311b8091b5346
SeaFalcon <seafalcon323@gmail.com>
2020-07-23 11:46:37 +0900

    디렉토리 생성 및 파일 정리

--- src/containers/RestaurantContainer.jsx
+++ src/containers/RestaurantContainer.jsx
@@ -2,17 +2,17 @@ import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import RestaurantDetail from './RestaurantDetail';
-import ReviewForm from './ReviewForm';
-import Reviews from './Reviews';
+import RestaurantDetail from '../presentational/RestaurantDetail';
+import ReviewForm from '../presentational/ReviewForm';
+import Reviews from '../presentational/Reviews';
 
 import {
   loadRestaurant,
   changeReviewField,
   sendReview,
-} from './actions';
+} from '../actions';
 
-import { get } from './utils';
+import { get } from '../utils';
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
