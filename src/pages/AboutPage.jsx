bac55b11912df8bc3c314b022bb4d087bcccf13a
daadaadaah <daadaadaah@gmail.com>
2020-07-26 07:17:28 +0900

    Apply Emotion to AboutPage component

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,29 @@
+import styled from '@emotion/styled';
+
 import React from 'react';
+
+const Container = styled.div({
+  marginTop: '2em',
+  marginBottom: '2em',
+  textAlign: 'center',
+});
+
+const Title = styled.h2({
+  margin: '1em',
+  textAlign: 'center',
+});
+
+const Description = styled.p({
+  margin: '0.4em 0',
+  textAlign: 'center',
+});
 
 export default function AboutPage() {
   return (
-    <div>
-      <h2>이 서비스에 대해서...</h2>
-      <p>이 서비스는 영국에서 시작되었습니다.</p>
-      <p>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</p>
-    </div>
+    <Container>
+      <Title>Let Eat Go 에 오신 것을 환영합니다!</Title>
+      <Description>이 서비스는 Codesoom에서 시작되었습니다.</Description>
+      <Description>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</Description>
+    </Container>
   );
 }
