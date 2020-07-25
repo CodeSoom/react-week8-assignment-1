8007be598b714075b979b3402b605cabe58f41b8
daadaadaah <daadaadaah@gmail.com>
2020-07-26 07:17:41 +0900

    Apply Emotion to HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,48 @@
+import styled from '@emotion/styled';
+
 import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import About from './AboutPage';
+
+const Container = styled.div({
+  padding: '1em',
+});
+
+const Menus = styled.div({
+  marginRight: '0.5em',
+  display: 'flex',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+});
+
+const Menu = styled.div({
+  padding: '1.5em',
+  margin: '0.4em',
+  borderRadius: '5%',
+  textAlign: 'center',
+  border: '1px solid #ff6659',
+  backgroundColor: '#ff6659',
+  '& a': {
+    textDecoration: 'none',
+    '& span': {
+      width: '100%',
+      fontSize: '20px',
+      color: '#eee',
+    },
+  },
+});
+
 export default function HomePage() {
   return (
-    <div>
-      <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
-    </div>
+    <Container>
+      <About />
+      <Menus>
+        <Menu><Link to="/about"><span>about</span></Link></Menu>
+        <Menu><Link to="/restaurants"><span>restaurants</span></Link></Menu>
+        <Menu><Link to="/login"><span>log in</span></Link></Menu>
+      </Menus>
+    </Container>
   );
 }
