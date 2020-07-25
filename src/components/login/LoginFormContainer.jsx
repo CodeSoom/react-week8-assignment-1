cbeaaf0924fcc8a86d16e38871cc047c32270163
sogoagain <imyong0@gmail.com>
2020-07-25 17:04:36 +0900

    Refactor - memoize LoginForm

--- src/components/login/LoginFormContainer.jsx
+++ src/components/login/LoginFormContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -13,19 +13,20 @@ import {
 
 import { get } from '../../utils/utils';
 
+
 export default function LoginFormContainer() {
   const dispatch = useDispatch();
 
   const loginFields = useSelector(get('loginFields'));
   const accessToken = useSelector(get('accessToken'));
 
-  const handleChange = ({ name, value }) => {
+  const handleChange = useCallback(({ name, value }) => {
     dispatch(changeLoginField({ name, value }));
-  };
+  }, [dispatch]);
 
-  const handleSubmit = () => {
+  const handleSubmit = useCallback(() => {
     dispatch(requestLogin());
-  };
+  }, [dispatch]);
 
   const handleClickLogout = () => {
     dispatch(logout());
