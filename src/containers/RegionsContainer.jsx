5419ae06308818490ab90f17f99565bb570b2b17
Paul An <anpaul0615@gmail.com>
2020-07-27 01:32:28 +0900

    React.memo + useCallback 적용

--- src/containers/RegionsContainer.jsx
+++ src/containers/RegionsContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -12,16 +12,17 @@ import { get } from '../utils';
 import MenuList from '../components/MenuList';
 import MenuItem from '../components/MenuItem';
 
+
 export default function RegionsContainer() {
   const dispatch = useDispatch();
 
   const regions = useSelector(get('regions'));
   const selectedRegion = useSelector(get('selectedRegion'));
 
-  function handleClick(regionId) {
+  const handleClick = useCallback((regionId) => {
     dispatch(selectRegion(regionId));
     dispatch(loadRestaurants());
-  }
+  }, [dispatch]);
 
   return (
     <MenuList>
