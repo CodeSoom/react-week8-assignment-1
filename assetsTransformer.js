4675cb10736176e141bba44087b95ad29b2ded35
Joylish <yeonzzu2@gmail.com>
2020-07-27 03:43:10 +0900

    updates assetsTransformer.js to pass test

--- assetsTransformer.js
+++ assetsTransformer.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  /* istanbul ignore next */
   process(filename) {
     return `module.exports = ${JSON.stringify(path.basename(filename))};`;
   },
