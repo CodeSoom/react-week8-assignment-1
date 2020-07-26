ff45d7541a3b5a949251f4c07bff2e20f26ead59
jiunkoo <jiunkoo0106@gmail.com>
2020-07-26 20:38:23 +0900

    PR 테스트 확인

--- src/styles/Button.jsx
+++ src/styles/Button.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 const Button = styled.button({
   padding: '0.5em 1em',
   border: '1px solid #CCC',
-  color: '#000',
+  color: '#bbb',
   textDecoration: 'none',
   cursor: 'pointer',
   fontSize: '16px',
