1185f36f5836d2b7b544555b73946e01a756ec06
yeonhwa <yeonhwa6717@gmail.com>
2020-07-27 23:03:49 +0900

    e2e 테스트 되돌려놓기
    - 통과시키려고 변경했는데 제대로된 테스트가 아닌거 같아서 되돌려놓음

--- test/login_page_test.js
+++ test/login_page_test.js
@@ -9,15 +9,18 @@ Scenario('로그인과 로그아웃', (I) => {
 
   I.click('Log In');
 
-  I.amOnPage('/restaurants');
-
-  I.see('Restaurants');
+  I.see('Log out');
 });
 
 Scenario('로그아웃', (I) => {
-  I.amOnPage('/logout');
-
   I.amOnPage('/login');
+
+  I.fillField('email', 'tester@example.com');
+  I.fillField('password', 'test');
+
+  I.click('Log In');
+
+  I.click('Log Out');
 
   I.see('Log In', 'a');
 });
