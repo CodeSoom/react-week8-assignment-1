c11da0b50c8a088517b211006cc2fafa01a09d6a
saseungmin <dbd02169@naver.com>
2020-11-09 22:32:08 +0900

    Delete redux-thunk import path

--- src/store.js
+++ src/store.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
-
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 
 import reducer from './slice';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer);
 
 export default store;
