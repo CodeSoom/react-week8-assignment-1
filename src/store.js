4e185328c6f579dae4a1b7738f377c59f53ee436
oceanide <oceanide@gmail.com>
2020-11-10 16:09:49 +0900

    Remove reducer.js and change import path of reducer
    This patch is included two things:
    - remove reducer.js
    - change to import path from reducer to slice

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 
 import thunk from 'redux-thunk';
 
-import reducer from './reducer';
+import reducer from './slice';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
