c7ea21807688618a56a0b13d40d4a81b9da3034e
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-10 22:23:54 +0900

    Add global stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './App.css';
+
 import {
   Switch,
   Route,
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
