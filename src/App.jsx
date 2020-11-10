ea6ba2734f8ac610ef1f73be54292a732c5b40c1
wjy5446 <wjy5446@gmail.com>
2020-11-10 22:04:40 +0900

    fix) 헤더 문구 수정
    테스트를 통과하기 위해서 "헤더" 문구로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
     <Container>
       <Header>
         <h1>
-          <Link to="/">EatGo</Link>
+          <Link to="/">헤더</Link>
         </h1>
       </Header>
       <Switch>
