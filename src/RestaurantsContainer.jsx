f7f59b73e67813be1b6ce3c2ec1ecffe1e0d3d25
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-11 21:37:33 +0900

    Set style of restaurant list

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import { get } from './utils';
+
+const List = styled.ul({
+  listStyle: 'none',
+  '& a': {
+    display: 'block',
+    padding: '.5rem',
+    paddingBottom: '3rem',
+    border: '1px solid gray',
+    marginBottom: '1rem',
+    fontSize: '1.8rem',
+    textDecoration: 'none',
+    color: '#111111',
+    transition: 'all .15s',
+    '&:hover': {
+      color: '#EEEEEE',
+    },
+  },
+});
 
 export default function RestaurantsContainer({ onClickRestaurant }) {
   const restaurants = useSelector(get('restaurants'));
@@ -15,7 +35,7 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
   }
 
   return (
-    <ul>
+    <List>
       {restaurants.map((restaurant) => (
         <li key={restaurant.id}>
           <a href="/restaurants/1" onClick={handleClick(restaurant)}>
@@ -23,6 +43,6 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
           </a>
         </li>
       ))}
-    </ul>
+    </List>
   );
 }
