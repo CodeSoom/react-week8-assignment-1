f555a472b892346acf0faf3297affe5152c6bae7
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-11 18:12:45 +0900

    Set style for regions

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import {
   selectRegion,
   loadRestaurants,
 } from './slice';
 
 import { get } from './utils';
+
+const Row = styled.ul({
+  display: 'flex',
+  listStyle: 'none',
+  padding: '1rem 1rem',
+  overflowY: 'hidden',
+  '&::-webkit-scrollbar': {
+    width: 0,
+  },
+});
+
+const Button = styled.button({
+  margin: '0 .5rem',
+  padding: '.5rem 1rem',
+  width: '7rem',
+});
 
 export default function RegionsContainer() {
   const dispatch = useDispatch();
@@ -21,22 +39,25 @@ export default function RegionsContainer() {
   }
 
   return (
-    <ul>
-      {regions.map((region) => (
-        <li key={region.id}>
-          <button
-            type="button"
-            onClick={() => handleClick(region.id)}
-          >
-            {region.name}
-            {selectedRegion ? (
-              <>
-                {region.id === selectedRegion.id ? '(V)' : null}
-              </>
-            ) : null}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h3>지역</h3>
+      <Row>
+        {regions.map((region) => (
+          <li key={region.id}>
+            <Button
+              type="button"
+              onClick={() => handleClick(region.id)}
+            >
+              {region.name}
+              {selectedRegion ? (
+                <>
+                  {region.id === selectedRegion.id ? '(V)' : null}
+                </>
+              ) : null}
+            </Button>
+          </li>
+        ))}
+      </Row>
+    </>
   );
 }
