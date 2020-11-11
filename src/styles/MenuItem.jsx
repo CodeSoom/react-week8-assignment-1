e0422f9edefada94c252737adb89c8aceeb64180
wjy5446 <wjy5446@gmail.com>
2020-11-11 21:58:44 +0900

    fix) MenuItem style 컴포넌트 수정
    - margin 값 수정

--- src/styles/MenuItem.jsx
+++ src/styles/MenuItem.jsx
@@ -4,14 +4,20 @@ const MenuItem = styled.li(({ active }) => ({
   marginRight: '1em',
   '& button': {
     padding: '.4em 1em',
-    border: '1px solid  #CCC',
-    background: active ? '#EEE' : 'transparent',
-    color: '#333',
+    border: 'none',
+    borderRadius: '.5em',
+    background: 'rgba(51, 51, 51, 0.05)',
+    fontWeight: 'bold',
+    fontSize: '1.5em',
+    color: active ? '#333' : '#6e6d7a',
     textDecoration: 'none',
     cursor: 'pointer',
     '&:hover': {
       fontWeight: 'bold',
-      color: '#000',
+      color: '#333',
+    },
+    '&:focus': {
+      outline: 'none',
     },
   },
 }));
