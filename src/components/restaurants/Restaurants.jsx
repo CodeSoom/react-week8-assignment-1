c83a5c296dd14f6719c8e9096f8e3f78e842334b
saseungmin <dbd02169@naver.com>
2020-11-11 23:39:23 +0900

    Update Restaurants apply styled components
    - create palette styles
    - Modified MenuItem

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+import palette from '../../lib/styles/palette';
+
+const RestaurantsBlock = styled.ul({
+  margin: 0,
+  padding: '.4em 0',
+  listStyle: 'none',
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const RestaurantItem = styled.li({
+  margin: '.4rem',
+  '& a': {
+    border: 'none',
+    borderRadius: '4px',
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    padding: '0.25rem 1rem',
+    color: 'white',
+    outline: 'none',
+    textDecoration: 'none',
+    background: palette.Teal[5],
+    '&:hover': {
+      background: palette.Teal[4],
+    },
+  },
+});
+
 const Restaurants = ({ restaurants, onClick }) => (
-  <ul>
+  <RestaurantsBlock>
     {restaurants.map((restaurant) => (
-      <li key={restaurant.id}>
+      <RestaurantItem key={restaurant.id}>
         <a href="/restaurants/1" onClick={onClick(restaurant)}>
           {restaurant.name}
         </a>
-      </li>
+      </RestaurantItem>
     ))}
-  </ul>
+  </RestaurantsBlock>
 );
 
 export default Restaurants;
--- src/lib/styles/MenuItem.jsx
+++ src/lib/styles/MenuItem.jsx
@@ -1,16 +1,20 @@
 import styled from '@emotion/styled';
 
+import palette from './palette';
+
 const MenuItem = styled.li(({ active }) => ({
   marginRight: '1em',
+  marginTop: '1rem',
   '& button': {
+    outline: 'none',
     padding: '.4em 1em',
     border: '1px solid #CCC',
-    background: active ? '#EEE' : 'transparent',
+    background: active ? palette.gray[3] : 'transparent',
     color: '#333',
     textDecoration: 'none',
     cursor: 'pointer',
     '&:hover': {
-      color: '#000',
+      color: palette.gray[9],
       fontWeight: 'bold',
     },
   },
