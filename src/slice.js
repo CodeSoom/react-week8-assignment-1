e44a5408d8e70e9c445c9647fa16aaaba9a53e81
Noah <mike1124@naver.com>
2020-11-11 19:52:34 +0900

    🎨 Add slice.js for apply redux toolkit

---
+++ src/slice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const slice = createSlice({
+  name: 'application',
+  initialState: {},
+  reducers: {},
+});
+
+export const {
+  actions,
+  reducer,
+} = slice;
+
+export default reducer;
