291cf5ee67046af140df0f67af415f12d4f624e9
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-11-11 21:55:30 +0900

    Fixed store.js
    It changed 'createStore' to 'configureStore' in 'RTK'

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from './slice';
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export default store;
