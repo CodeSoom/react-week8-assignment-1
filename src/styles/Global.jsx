376834ee66b341d6b8fcc56fea3d93840630941a
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-12 22:59:43 +0900

    Set basic font

--- src/styles/Global.jsx
+++ src/styles/Global.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { Global as GlobalStyle, css } from '@emotion/core';
 
 const style = css`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
   *,
   *::before,
   *::after {
     margin: 0;
     padding: 0;
     box-sizing: inherit;
+    font-family: inherit;
   }
 
   html {
@@ -19,6 +22,7 @@ const style = css`
   body {
     background-color: gray;
     font-size: 1.6rem;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 `;
 
