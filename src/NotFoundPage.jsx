50d94bf5cd0e945282feebf07605f45647ee4c78
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-12 20:10:22 +0900

    Change the position of style declarations in Reviews and NotFoundPage.
    Change the position of style declaration from inside component to outside.

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -4,16 +4,16 @@ import styled from '@emotion/styled';
 
 import Container from './Container';
 
-export default function NotFoundPage() {
-  const NotFoundText = styled.p({
-    backgroundColor: 'rgba(255,0,0,0.3)',
-    backdropFilter: 'saturate(180%) blur(30px)',
-    color: '#fff',
-    fontSize: '5em',
-    padding: '0.5em 1em',
-    borderRadius: '0.5em',
-  });
+const NotFoundText = styled.p({
+  backgroundColor: 'rgba(255,0,0,0.3)',
+  backdropFilter: 'saturate(180%) blur(30px)',
+  color: '#fff',
+  fontSize: '5em',
+  padding: '0.5em 1em',
+  borderRadius: '0.5em',
+});
 
+export default function NotFoundPage() {
   return (
     <Container>
       <NotFoundText>
--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
-const Reviews = React.memo(({ reviews, scrollY=0 }) => {
+const ReviewItem = styled.ul({
+  listStyle: 'none',
+  '& li': {
+    color: '#000',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    margin: '1em',
+    padding: '0.5em 2em',
+    '& span': {
+      fontWeight: 'bold',
+    },
+    boxShadow: '2px 3px 20px rgba(0,0,0,0.3)',
+    transform: `translateY(-${({ scrollY }) => scrollY || 0}px)`,
+  },
+});
+
+const Reviews = React.memo(({ reviews, scrollY }) => {
   if (!reviews || !reviews.length) {
     return null;
   }
 
   const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
 
-  const ReviewItem = styled.ul({
-    listStyle: 'none',
-    '& li': {
-      color: '#000',
-      backgroundColor: 'rgba(255,255,255,0.5)',
-      margin: '1em',
-      padding: '0.5em 2em',
-      '& span': {
-        fontWeight: 'bold',
-      },
-      boxShadow: '2px 3px 20px rgba(0,0,0,0.3)',
-      transform: `translateY(-${scrollY}px)`,
-    },
-  });
-
   return (
-    <ReviewItem>
+    <ReviewItem scrollY={scrollY}>
       {sortedReviews.map((review) => (
         <li key={review.id}>
           <span>
