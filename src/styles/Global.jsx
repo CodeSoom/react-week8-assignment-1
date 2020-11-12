21ef79c232de6da358dd1bfb081519c06b489289
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-12 21:57:23 +0900

    Extract complex string into constant
    for readability

--- src/styles/Global.jsx
+++ src/styles/Global.jsx
@@ -2,30 +2,28 @@ import React from 'react';
 
 import { Global as GlobalStyle, css } from '@emotion/core';
 
-const fontNormal = '1.6rem';
+const style = css`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: gray;
+    font-size: 1.6rem;
+  }
+`;
 
 export default function Global() {
   return (
-    <GlobalStyle
-      styles={css`
-        *,
-        *::before,
-        *::after {
-          margin: 0;
-          padding: 0;
-          box-sizing: inherit;
-        }
-
-        html {
-          box-sizing: border-box;
-          font-size: 62.5%;
-        }
-
-        body {
-          background-color: gray;
-          font-size: ${fontNormal};
-        }
-      `}
-    />
+    <GlobalStyle styles={style} />
   );
 }
