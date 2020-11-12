21781aaecce019f758e03b0739b216decae84991
Noah <mike1124@naver.com>
2020-11-12 21:10:27 +0900

    🎨 Decorate About Page
    Decorate about page and Add style components

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import MenuTitle from './styles/MenuTitle';
+
+import Text from './styles/Text';
+
 export default function AboutPage() {
   return (
     <div>
-      <h2>이 서비스에 대해서...</h2>
-      <p>이 서비스는 영국에서 시작되었습니다.</p>
-      <p>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</p>
+      <MenuTitle>ABOUT</MenuTitle>
+      <Text>이 서비스는 영국에서 시작되었습니다.</Text>
+      <Text>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</Text>
     </div>
   );
 }
---
+++ src/styles/Button.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button({
+  padding : 15,
+  backgroundColor : '#F3FFB9'
+  },
+});
+
+export default Button;
