236cf5aff0d16035f34b0fcc1c5ee352220401f0
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-11-14 17:24:07 +0900

    UI 추가
    If users not selected region and category, shows message recommending selection.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import Restaurants from './Restaurants';
 
 import { get } from './utils';
+
+const RestaurantsArea = styled.article({
+  '& h3': {
+    color: '#3c4348',
+  },
+  '& p': {
+    color: '#8b9aa7',
+  },
+});
 
 export default function RestaurantsContainer({ onClickRestaurant }) {
   const restaurants = useSelector(get('restaurants'));
@@ -17,12 +28,16 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
   }
 
   return (
-    <>
+    <RestaurantsArea>
       <h3>레스토랑</h3>
-      <Restaurants
-        restaurants={restaurants}
-        onClick={handleClick}
-      />
-    </>
+      {restaurants.length ? (
+        <Restaurants
+          restaurants={restaurants}
+          onClick={handleClick}
+        />
+      ) : (
+        <p>지역과 카테고리를 선택해주세요!!!</p>
+      )}
+    </RestaurantsArea>
   );
 }
