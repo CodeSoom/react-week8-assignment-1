c006539c28d8c8cef3b68f6cdbeb08c260a5181d
EL <ickyicky@gmail.com>
2021-03-15 20:38:08 +0900

    stye: 헤더 영역 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
 
 import { useDispatch } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import LoginPage from './LoginPage';
@@ -19,6 +21,18 @@ import { setAccessToken } from './slice';
 
 import { loadItem } from './services/storage';
 
+const Header = styled.div({
+  width: '100%',
+  textAlign: 'center',
+  padding: '0.1em',
+  backgroundColor: '#e07a5f',
+  '& h1 a': {
+    textDecoration: 'none',
+    margin: '0',
+    color: 'WHITE',
+  },
+});
+
 export default function App() {
   const dispatch = useDispatch();
 
@@ -29,11 +43,11 @@ export default function App() {
 
   return (
     <div>
-      <header>
+      <Header>
         <h1>
           <Link to="/">헤더 영역</Link>
         </h1>
-      </header>
+      </Header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
