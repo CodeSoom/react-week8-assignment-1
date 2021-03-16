160d13ac14eb1d90994a382b9124021ab6f08c54
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-16 20:46:58 +0900

    AboutPage 스타일링 완료!
    내용이 너무 빈약해서 제가 조금 첨부했습니다

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+import PageMain from './PageMain';
+
+const AboutPageParagraph = styled.div({
+  margin: '40px 0 0 27px',
+  fontSize: '18px',
+  textAlign: 'center',
+  height: '400px',
+  width: '270px',
+  borderTop: '4px solid teal',
+  borderBottom: '4px solid teal',
+  lineHeight: '30px',
+});
+
 export default function AboutPage() {
   return (
-    <div>
+    <PageMain>
       <h2>이 서비스에 대해서...</h2>
-      <p>이 서비스는 영국에서 시작되었습니다.</p>
-      <p>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</p>
-    </div>
+      <AboutPageParagraph>
+        <p>이 서비스는 영국에서 시작되었습니다.</p>
+        <p>이 서비스를 보셨다면 </p>
+        <p>주변에 있는 20명에게 추천하셔야 합니다.</p>
+        <p>만약 1주일안에 추천하지 않으면</p>
+        <p>아무일도 일어나지 않습니다.</p>
+        <p>추천하기 싫으시면</p>
+        <p>그냥 않하시면 됩니다.</p>
+        <p>죄송합니다..</p>
+      </AboutPageParagraph>
+    </PageMain>
   );
 }
