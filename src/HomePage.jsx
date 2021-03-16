16c474af50d1e94ae7f82d1014d1861f6557c0e4
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-16 19:32:55 +0900

    HomePage emotion으로 스타일링

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 import { Link } from 'react-router-dom';
+
+const PageMain = styled.div({
+  height: '550px',
+  '& h2': {
+    fontSize: '30px',
+    textAlign: 'center',
+  },
+});
+
+const Item = styled.li({
+  padding: '35px 0',
+  display: 'flex',
+  justifyContent: 'center',
+  '& a': {
+    display: 'block',
+    textDecoration: 'none',
+    position: 'inline-block',
+    width: '70%',
+    backgroundColor: 'teal',
+    color: 'white',
+    borderRadius: '10px',
+    padding: '5px 0',
+  },
+});
+
+const List = styled.ul({
+  listStyle: 'none',
+  paddingLeft: '0px',
+  fontSize: '30px',
+  textAlign: 'center',
+});
 
 export default function HomePage() {
   return (
-    <div>
+    <PageMain>
       <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
-    </div>
+      <List>
+        <Item><Link to="/about">About</Link></Item>
+        <Item><Link to="/login">Log in</Link></Item>
+        <Item><Link to="/restaurants">Restaurants</Link></Item>
+        <Item><Link to="/xxx">Go to hell</Link></Item>
+      </List>
+    </PageMain>
   );
 }
