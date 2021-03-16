e686a19520b626b6d2d61e38ad1c05d019be1cef
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-03-16 11:17:50 +0900

    apply style to list of regions and categories

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -9,6 +9,9 @@ import {
 
 import { get } from './utils';
 
+import MenuList from './MenuList';
+import MenuItem from './MenuItem';
+
 export default function CategoriesContainer() {
   const dispatch = useDispatch();
 
@@ -21,9 +24,12 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <ul>
+    <MenuList>
       {categories.map((category) => (
-        <li key={category.id}>
+        <MenuItem 
+          key={category.id}
+          active={selectedCategory && category.id === selectedCategory.id}
+        >
           <button
             type="button"
             onClick={() => handleClick(category.id)}
@@ -35,8 +41,8 @@ export default function CategoriesContainer() {
               </>
             ) : null}
           </button>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
