ea2b693746139a0eabf2dafe0e6f25351c42c21b
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-16 15:43:18 +0900

    React.memo 제거
    React.memo는 성능최적화를 위해서 사용하지만 간단한 컴포넌트는 굳이 사용할 필요 없음

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function NotFoundPage() {
+export default function NotFoundPage() {
   return (
     <p>404 Not Found</p>
   );
 }
-
-export default React.memo(NotFoundPage);
