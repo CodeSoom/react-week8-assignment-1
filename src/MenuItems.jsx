4a0f2dd7bba700b7f9cd943874801b7af55831bb
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-03-17 16:39:32 +0900

    design menu items

--- src/MenuItems.jsx
+++ src/MenuItems.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const List = styled.ul({
+  display: 'flex',
+  justifyContent: 'center',
+  listStyle: 'none',
+  padding: 0,
+});
+
+const Item = styled.li({
+  textAlign: 'center',
+  margin: '0px 7px',
+  fontFamily: '"Song Myung", serif',
+  fontSize: '20px',
+  padding: '5px 0',
+  width: '110px',
+  borderRadius: '5px',
+  border: 'none',
+  boxShadow: '0px 0px 5px white',
+  background: 'rgba(255, 255, 255, 0.5)',
+  color: '#5B4230',
+  textDecoration: 'none',
+})
+
+
 export default function MenuItems({ menuItems }) {
   if (!(menuItems || []).length) {
     return (
@@ -8,12 +33,12 @@ export default function MenuItems({ menuItems }) {
   }
 
   return (
-    <ul>
+    <List>
       {menuItems.map((menuItem) => (
-        <li key={menuItem.id}>
+        <Item key={menuItem.id}>
           {menuItem.name}
-        </li>
+        </Item>
       ))}
-    </ul>
+    </List>
   );
 }
