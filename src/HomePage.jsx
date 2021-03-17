21747af3e2595414ea787de09b7068c5f4886720
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-17 22:10:40 +0900

    Set style in HomePage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+const HomePageWrapper = styled.div`
+
+& > h2 {
+padding-bottom:15px;
+}
+
+& > ul {
+display:flex;
+width:20%;
+flex-direction:row;
+justify-content:space-between
+}
+`;
+
 export default function HomePage() {
   return (
-    <div>
+    <HomePageWrapper>
       <h2>Home</h2>
       <ul>
         <li><Link to="/about">About</Link></li>
@@ -12,6 +28,6 @@ export default function HomePage() {
         <li><Link to="/restaurants">Restaurants</Link></li>
         <li><Link to="/xxx">멸망의 길</Link></li>
       </ul>
-    </div>
+    </HomePageWrapper>
   );
 }
