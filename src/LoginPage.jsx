17b780bc6b1d96a10e2e9caf69035eb240fc67af
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-03-17 10:35:12 +0900

    design loginpage title

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import LoginFormContainer from './LoginFormContainer';
 
+import styled from '@emotion/styled';
+
+const Title = styled.h2({
+  fontSize: '35px',
+  margin: '30px 10px',
+  padding: '10px 20px',
+  borderBottom: '2px solid rgba(249, 253, 252, 0.5)',
+  color: '#F9FDFC',
+});
+
 export default function LoginPage() {
   return (
     <div>
-      <h2>Log In</h2>
+      <Title>Log In</Title>
       <LoginFormContainer />
     </div>
   );
