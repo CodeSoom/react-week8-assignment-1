d566f44e266ec5cd406118e57597aaafbf797524
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-17 19:08:46 +0900

    Button.jsx 생성
    중복되는 styledComponent

---
+++ src/Button.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button({
+  fontFamily: 'Raleway',
+  fontSize: '20px',
+  color: 'white',
+  width: '170px',
+  height: '50px',
+  borderRadius: '10px',
+  backgroundColor: 'teal',
+  border: 'none',
+  margin: '70px 0 0 80px',
+  cursor: 'pointer',
+});
+
+export default Button;
