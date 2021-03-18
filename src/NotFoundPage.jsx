cc3ca9fd8900f2bff2305e96d637735e9bfe5665
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-03-18 16:39:47 +0900

    design not found page

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const Title = styled.p({
+  textAlign: 'center',
+  fontSize: '80px',
+  fontFamily: '"Tillana", cursive',
+  fontWeight: '600',
+  margin: '30px 10px',
+  padding: '10px 20px',
+  color: '#F9FDFC',
+});
+
 export default function NotFoundPage() {
   return (
-    <p>404 Not Found</p>
+    <Title>404 Not Found</Title>
   );
 }
