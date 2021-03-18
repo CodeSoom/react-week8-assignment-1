1b9fc265fcddcd895ab568a11338a4cfcc3e2778
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-03-18 16:39:26 +0900

    change restaurant detail

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -6,9 +6,8 @@ import styled from '@emotion/styled';
 
 const Title = styled.h2({
   fontSize: '35px',
-  margin: '30px 10px',
-  padding: '10px 20px',
-  borderBottom: '2px solid rgba(249, 253, 252, 0.5)',
+  margin: '10px 10px',
+  padding: '5px',
   color: '#F9FDFC',
   fontFamily: '"Song Myung", serif',
 });
@@ -17,14 +16,22 @@ const Text = styled.p({
   fontSize: '25px',
   color: '#F9FDFC',
   margin: '10px 20px',
+  padding: '5px',
   fontFamily: '"Song Myung", serif',
 });
+
+const RestaurantDetailContainer = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-end',
+  borderBottom: '2px solid rgba(249, 253, 252, 0.5)',
+})
 
 const Subtitle = styled.h3({
   fontFamily: '"Song Myung", serif',
   fontSize: '30px',
   margin: '10px 20px',
-  marginTop: '30px',
+  marginTop: '10px',
   color: '#F9FDFC',
   opacity: '0.8',
   textAlign: 'center',
@@ -35,12 +42,14 @@ export default function RestaurantDetail({ restaurant }) {
 
   return (
     <div>
-      <Title>{name}</Title>
-      <Text>
-        주소:
-        {' '}
-        {address}
-      </Text>
+      <RestaurantDetailContainer>
+        <Title>{name}</Title>
+        <Text>
+          주소:
+          {' '}
+          {address}
+        </Text>
+      </RestaurantDetailContainer>
       <Subtitle>메뉴</Subtitle>
       <MenuItems menuItems={menuItems} />
     </div>
