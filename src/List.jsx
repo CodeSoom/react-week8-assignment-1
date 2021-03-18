08396781cf901ceb16f066cb5bba96e1faa51ec0
yoonhe <yhehe228@gmail.com>
2021-03-18 21:09:37 +0900

    List, ListItem의 UI,UX를 개선합니다

--- src/List.jsx
+++ src/List.jsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 const List = styled.ul({
   display: 'flex',
   flexWrap: 'wrap',
+  gap: '1rem',
 });
 
 export default List;
--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -2,19 +2,21 @@
 import styled from '@emotion/styled';
 
 const ListItem = styled.li(({ active }) => ({
-  '& + &': {
-    marginLeft: '0.5rem',
-  },
+  width: '23%',
   'button, a': {
     display: 'block',
+    width: '100%',
     padding: '0.5rem 1.1em',
     fontSize: '1rem',
+    textAlign: 'center',
     color: active && '#fff',
     border: '1px solid #dcdcdc',
+    boxSizing: 'border-box',
     borderRadius: '1em',
     background: active && '#8989ba',
     cursor: 'pointer',
     transition: '0.2s linear',
+
     '&:hover': {
       color: '#fff',
       background: '#8989ba',
