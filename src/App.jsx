4ac4f141ec9b35f51f84956562adddabc1f6a6d3
yoonhe <yhehe228@gmail.com>
2021-03-20 00:39:12 +0900

    로고 컬러값을 변경합니다

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,20 @@ import NotFoundPage from './NotFoundPage';
 import { setAccessToken } from './slice';
 
 import { loadItem } from './services/storage';
+import green from '../fixtures/color';
 
 const Container = styled.div({
-  margin: '0 auto',
-  width: '90%',
+
 });
 
 const Header = styled.header({
   margin: '2rem 0',
   '& h1 a': {
     display: 'block',
-    color: '#a18cd1',
+    color: `${green}`,
     fontSize: '2rem',
     textAlign: 'center',
   },
-});
-
-const Body = styled.div({
-  padding: '1rem',
 });
 
 export default function App() {
@@ -55,16 +51,14 @@ export default function App() {
           <Link to="/">레스토랑 검색 서비스</Link>
         </h1>
       </Header>
-      <Body>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route exact path="/restaurants" component={RestaurantsPage} />
-          <Route path="/restaurants/:id" component={RestaurantPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </Body>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route exact path="/restaurants" component={RestaurantsPage} />
+        <Route path="/restaurants/:id" component={RestaurantPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </Container>
   );
 }
