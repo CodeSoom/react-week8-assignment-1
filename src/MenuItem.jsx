336bb32bd1279064ce5999291a4608b55b1642d8
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-20 21:58:28 +0900

    식당과 분류 버튼을 누르는 순간 색상을 반전시킵니다.

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -9,15 +9,15 @@ const MenuItem = styled.li({
     border: 0,
     borderRadius: 5,
     cursor: 'pointer',
+    '&:active': {
+      color: '#FFABAB',
+      backgroundColor: '#FFF5BA',
+      textDecoration: 'none',
+    },
   },
   '&:hover': {
     fontWeight: 'bold',
     color: '#F3FFE3',
-  },
-  '&:active': {
-    color: '#B28DFF',
-    backgroundColor: '#DCD3FF',
-    textDecoration: 'none',
   },
 });
 
