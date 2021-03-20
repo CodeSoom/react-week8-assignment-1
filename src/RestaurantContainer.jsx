a96bfea216efaff335832716994d8160fe887cbf
yoonhe <yhehe228@gmail.com>
2021-03-20 16:57:05 +0900

    로딩바 UI,UX를 개선합니다
    svg 아이콘 사용을 하기위해 react-icons 라이브러를 설치했습니다.
    아이콘에 css animation을 적용하기위해 @emotion/css 라이브러리를 설치했습니다.
    
    loading 아이콘이 있지만 시각장애인들을 위해 loading... 텍스트를 지우지 않았습니다

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -2,6 +2,11 @@ import React, { useCallback, useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { VscLoading } from 'react-icons/vsc';
+
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/css';
+
 import RestaurantDetail from './RestaurantDetail';
 import ReviewForm from './ReviewForm';
 import Reviews from './Reviews';
@@ -13,6 +18,42 @@ import {
 } from './slice';
 
 import { get } from './utils';
+
+const Loading = styled.div({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100%',
+  background: 'rgba(0,0,0,0.3)',
+  '& > p': {
+    fontSize: 0,
+    padding: '3rem',
+    borderRadius: '0.5rem',
+    border: '1px solid #dcdcdc',
+    background: '#fff',
+  },
+});
+
+const spin = keyframes({
+  from: {
+    transform: 'rotate(0deg)',
+  },
+  to: {
+    transform: 'rotate(360deg)',
+  },
+});
+
+const Icon = styled.span({
+  '& svg': {
+    fontSize: '2rem',
+    animation: `${spin} infinite 2s linear`,
+  },
+});
+
 
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
@@ -35,7 +76,16 @@ export default function RestaurantContainer({ restaurantId }) {
 
   if (!restaurant) {
     return (
-      <p>Loading...</p>
+      <Loading>
+        <Loading>
+          <p>
+            Loading...
+            <Icon>
+              <VscLoading />
+            </Icon>
+          </p>
+        </Loading>
+      </Loading>
     );
   }
 
