c1e2d3f7d20e2df81c58ea0361296c5fa52a9dfd
seong-dodo <dyrnxm52@hanmail.net>
2021-06-22 11:00:20 +0900

    Emotion 라이브러리 이용해서 꾸미기: index.html, HomePage컴포넌트
    - index.html에 기본 한글폰트 추가하기
    - HomePage컴포넌트 스타일 설정하기

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,15 +1,47 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+const Container = styled.div({
+  '& h2': {
+    fontsize: '1.5em',
+    margin: 0,
+    padding: '.5em 2.5em',
+    color: '#311800',
+  },
+});
+
+const List = styled.ul({
+  display: 'flex',
+  margin: 0,
+  padding: '0 2.5em',
+  listStyle: 'none',
+});
+
+const Item = styled.li({
+  margin: 0,
+  padding: '0 1.5em',
+  fontsize: '.5em',
+  '& a': {
+    color: '#55462b',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#311800',
+      fontWeight: '600',
+    },
+  },
+});
+
 export default function HomePage() {
   return (
-    <div>
+    <Container>
       <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
-    </div>
+      <List>
+        <Item><Link to="/about">About</Link></Item>
+        <Item><Link to="/login">Log in</Link></Item>
+        <Item><Link to="/restaurants">Restaurants</Link></Item>
+        <Item><Link to="/xxx">멸망의 길</Link></Item>
+      </List>
+    </Container>
   );
 }
