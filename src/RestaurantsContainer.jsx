01954d888e3b94c50d56d00d68e923640ace3333
최한울 <hanul.choi95@nts-corp.com>
2021-06-22 23:12:05 +0900

    Style RestaurantsContainer
    - 지역과 카테고리 선택 시 나타나는 식당 목록에 스타일을 입혔습니다.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -2,6 +2,31 @@ import { useSelector } from 'react-redux';
 
 import { get } from './utils';
 
+import styled from '@emotion/styled';
+
+const List = styled.ul({
+  display: 'flex',
+  listStyle: 'none',
+  padding: '.4em 0',
+  margin: '0 auto',
+});
+
+const Item = styled.li({
+  marginRight: '1em',
+  '& a': {
+    padding: '.4em 1em',
+    border: '1px solid #CCC',
+    background: 'transparent',
+    color: 'green',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#000',
+      fontWeight: 'bold',
+      cursor: 'pointer',
+    },
+  },
+})
+
 export default function RestaurantsContainer({ onClickRestaurant }) {
   const restaurants = useSelector(get('restaurants'));
 
@@ -13,14 +38,18 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
   }
 
   return (
-    <ul>
-      {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
-          <a href="/restaurants/1" onClick={handleClick(restaurant)}>
-            {restaurant.name}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <>
+      {(restaurants.length !== 0) && <p>식당을 선택하세요</p>}
+      <List>
+        {restaurants.map((restaurant) => (
+          <Item key={restaurant.id}>
+            <a href="/restaurants/1" onClick={handleClick(restaurant)}>
+              {restaurant.name}
+            </a>
+          </Item>
+        ))}
+      </List>
+    </>
+
   );
 }
