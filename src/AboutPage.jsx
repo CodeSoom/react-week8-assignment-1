fc7a4d2d52d83bd703dd4fbd2c802cdbcfa11c7d
seong-dodo <dyrnxm52@hanmail.net>
2021-06-22 16:16:03 +0900

    Emotion 라이브러리 이용해서 꾸미기: AboutPage, NotFoundPage

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,9 +1,19 @@
+import styled from '@emotion/styled';
+import Container from './Container';
+import Title from './Title';
+
+const Paragraph = styled.p({
+  margin: 0,
+  padding: '.5em 4em',
+  color: '#311800',
+});
+
 export default function AboutPage() {
   return (
-    <div>
-      <h2>이 서비스에 대해서...</h2>
-      <p>이 서비스는 영국에서 시작되었습니다.</p>
-      <p>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</p>
-    </div>
+    <Container>
+      <Title>이 서비스에 대해서...</Title>
+      <Paragraph>이 서비스는 영국에서 시작되었습니다.</Paragraph>
+      <Paragraph>이 서비스를 보셨다면 주변에 있는 20명에게 추천하셔야 합니다.</Paragraph>
+    </Container>
   );
 }
--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -1,5 +1,16 @@
+import styled from '@emotion/styled';
+
+const ErrorParagraph = styled.p({
+  fontSize: '1em',
+  margin: '0 auto',
+  padding: '0',
+  color: '#d16b60',
+  textAlign: 'center',
+  lineHeight: '10em',
+});
+
 export default function NotFoundPage() {
   return (
-    <p>404 Not Found</p>
+    <ErrorParagraph>404 Not Found</ErrorParagraph>
   );
 }
