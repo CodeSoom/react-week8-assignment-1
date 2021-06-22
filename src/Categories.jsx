c54cf0ba41baa088bbb6c858424199b5400dff76
dngwoodo <dngwoodo@gmail.com>
2021-06-22 23:29:55 +0900

    refactor: Change export default position

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,21 +1,25 @@
 import { memo } from 'react';
 
-export default memo(({ categories, selectedCategory, onClick }) => (
-  <ul>
-    {categories.map((category) => (
-      <li key={category.id}>
-        <button
-          type="button"
-          onClick={() => onClick(category.id)}
-        >
-          {category.name}
-          {selectedCategory ? (
-            <>
-              {category.id === selectedCategory.id ? '(V)' : null}
-            </>
-          ) : null}
-        </button>
-      </li>
-    ))}
-  </ul>
-));
+function Categories({ categories, selectedCategory, onClick }) {
+  return (
+    <ul>
+      {categories.map((category) => (
+        <li key={category.id}>
+          <button
+            type="button"
+            onClick={() => onClick(category.id)}
+          >
+            {category.name}
+            {selectedCategory ? (
+              <>
+                {category.id === selectedCategory.id ? '(V)' : null}
+              </>
+            ) : null}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default memo(Categories);
