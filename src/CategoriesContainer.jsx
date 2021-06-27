a45501fe3beb0b2090c134a3a66aa214251a89ed
hanvit7 <hanvit7@gmail.com>
2021-06-27 17:37:24 +0900

    restaurants 지역, 카테고리 리스트에 css 적용

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,11 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import {
-  selectCategory,
-  loadRestaurants,
-} from './actions';
+import { selectCategory, loadRestaurants } from './actions';
 
 import { get } from './utils';
+
+import MenuList from './MenuList';
+import MenuItem from './MenuItem';
 
 export default function CategoriesContainer() {
   const dispatch = useDispatch();
@@ -19,22 +19,20 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <ul>
+    <MenuList>
       {categories.map((category) => (
-        <li key={category.id}>
-          <button
-            type="button"
-            onClick={() => handleClick(category.id)}
-          >
+        <MenuItem
+          key={category.id}
+          active={selectedCategory && category.id === selectedCategory.id}
+        >
+          <button type="button" onClick={() => handleClick(category.id)}>
             {category.name}
             {selectedCategory ? (
-              <>
-                {category.id === selectedCategory.id ? '(V)' : null}
-              </>
+              <>{category.id === selectedCategory.id ? '(V)' : null}</>
             ) : null}
           </button>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import {
-  selectRegion,
-  loadRestaurants,
-} from './actions';
+import MenuList from './MenuList';
+import MenuItem from './MenuItem';
+
+import { selectRegion, loadRestaurants } from './actions';
 
 import { get } from './utils';
 
@@ -19,22 +19,20 @@ export default function RegionsContainer() {
   }
 
   return (
-    <ul>
+    <MenuList>
       {regions.map((region) => (
-        <li key={region.id}>
-          <button
-            type="button"
-            onClick={() => handleClick(region.id)}
-          >
+        <MenuItem
+          key={region.id}
+          active={selectedRegion && region.id === selectedRegion.id}
+        >
+          <button type="button" onClick={() => handleClick(region.id)}>
             {region.name}
             {selectedRegion ? (
-              <>
-                {region.id === selectedRegion.id ? '(V)' : null}
-              </>
+              <>{region.id === selectedRegion.id ? '(V)' : null}</>
             ) : null}
           </button>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
