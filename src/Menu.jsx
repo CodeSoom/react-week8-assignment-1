91583e881fdfa02668cbb7dffc234dd3f573aa00
kooku <kooku@kakao.com>
2021-10-06 22:55:02 +0900

    Change MenuList to Menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 
-const MenuList = styled.ul({
+const Menu = styled.ul({
   display: 'flex',
   margin: 0,
   padding: '.4em 0',
   listStyle: 'none',
 });
 
-export default MenuList;
+export default Menu;
