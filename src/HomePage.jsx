eddede795696a663e5dbeb601945f127109700d9
passwd10 <parkinseo94@naver.com>
2021-10-08 20:52:53 +0900

    feat: HomePage 스타일링

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,15 +1,37 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
 export default function HomePage() {
   return (
     <div>
-      <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
+      <Title>Home</Title>
+      <Ul>
+        <Li><Link to="/about">About</Link></Li>
+        <Li><Link to="/login">Log in</Link></Li>
+        <Li><Link to="/restaurants">Restaurants</Link></Li>
+        <Li><Link to="/xxx">멸망의 길</Link></Li>
+      </Ul>
     </div>
   );
 }
+
+const Title = styled.h2`
+  font-size: 2em;
+  text-align: center;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 2em;
+`;
+
+const Li = styled.li`
+  padding: 1em;
+
+  &:hover {
+    color: white;
+    background-color: black;
+  }
+`;
