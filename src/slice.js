445737e1abc889de064b981b490007f6039f8009
Lerry <lerry597@gmail.com>
2022-01-19 18:11:54 +0900

    [refactor] Redux Toolkit

--- src/slice.js
+++ src/slice.js
@@ -2,6 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { equal } from './utils';
 
+import {
+  fetchRegions,
+  fetchCategories,
+  fetchRestaurants,
+  fetchRestaurant,
+  postLogin,
+  postReview,
+} from './services/api';
+
+import { saveItem } from './services/storage';
+
 const initialReviewFields = {
   score: '',
   description: '',
@@ -25,36 +36,37 @@ const { actions, reducer } = createSlice({
       ...initialReviewFields,
     },
   },
+
   reducers: {
-    setRegions(state, { payload: { regions } }) {
+    setRegions(state, { payload: regions }) {
       return {
         ...state,
         regions,
       };
     },
 
-    setCategories(state, { payload: { categories } }) {
+    setCategories(state, { payload: categories }) {
       return {
         ...state,
         categories,
       };
     },
 
-    setRestaurants(state, { payload: { restaurants } }) {
+    setRestaurants(state, { payload: restaurants }) {
       return {
         ...state,
         restaurants,
       };
     },
 
-    setRestaurant(state, { payload: { restaurant } }) {
+    setRestaurant(state, { payload: restaurant }) {
       return {
         ...state,
         restaurant,
       };
     },
 
-    selectRegion(state, { payload: { regionId } }) {
+    selectRegion(state, { payload: regionId }) {
       const { regions } = state;
       return {
         ...state,
@@ -62,7 +74,7 @@ const { actions, reducer } = createSlice({
       };
     },
 
-    selectCategory(state, { payload: { categoryId } }) {
+    selectCategory(state, { payload: categoryId }) {
       const { categories } = state;
       return {
         ...state,
@@ -80,7 +92,7 @@ const { actions, reducer } = createSlice({
       };
     },
 
-    setAccessToken(state, { payload: { accessToken } }) {
+    setAccessToken(state, { payload: accessToken }) {
       return {
         ...state,
         accessToken,
@@ -113,7 +125,7 @@ const { actions, reducer } = createSlice({
       };
     },
 
-    setReviews(state, { payload: { reviews } }) {
+    setReviews(state, { payload: reviews }) {
       const { restaurant } = state;
 
       return {
@@ -127,7 +139,91 @@ const { actions, reducer } = createSlice({
   },
 });
 
-export {
-  actions,
-  reducer,
-};
+export const {
+  setRegions,
+  setCategories,
+  setRestaurants,
+  setRestaurant,
+  selectRegion,
+  selectCategory,
+  changeLoginField,
+  setAccessToken,
+  logout,
+  changeReviewField,
+  clearReviewFields,
+  setReviews,
+} = actions;
+
+export default reducer;
+
+export function loadInitialData() {
+  return async (dispatch) => {
+    const regions = await fetchRegions();
+    dispatch(setRegions(regions));
+
+    const categories = await fetchCategories();
+    dispatch(setCategories(categories));
+  };
+}
+
+export function loadRestaurants() {
+  return async (dispatch, getState) => {
+    const {
+      selectedRegion: region,
+      selectedCategory: category,
+    } = getState();
+
+    if (!region || !category) {
+      return;
+    }
+
+    const restaurants = await fetchRestaurants({
+      regionName: region.name,
+      categoryId: category.id,
+    });
+    dispatch(setRestaurants(restaurants));
+  };
+}
+
+export function loadRestaurant({ restaurantId }) {
+  return async (dispatch) => {
+    dispatch(setRestaurant(null));
+
+    const restaurant = await fetchRestaurant({ restaurantId });
+
+    dispatch(setRestaurant(restaurant));
+  };
+}
+
+export function requestLogin() {
+  return async (dispatch, getState) => {
+    const { loginFields: { email, password } } = getState();
+
+    const accessToken = await postLogin({ email, password });
+
+    saveItem('accessToken', accessToken);
+
+    dispatch(setAccessToken(accessToken));
+  };
+}
+
+export function loadReview({ restaurantId }) {
+  return async (dispatch) => {
+    const restaurant = await fetchRestaurant({ restaurantId });
+
+    dispatch(setReviews(restaurant.reviews));
+  };
+}
+
+export function sendReview({ restaurantId }) {
+  return async (dispatch, getState) => {
+    const { accessToken, reviewFields: { score, description } } = getState();
+
+    await postReview({
+      accessToken, restaurantId, score, description,
+    });
+
+    dispatch(loadReview({ restaurantId }));
+    dispatch(clearReviewFields());
+  };
+}
