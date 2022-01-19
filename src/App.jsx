250edc81c982c75649d4e1503b3f4bad0e220d5d
Rok <ur_0ke@inflearn.com>
2022-01-19 23:20:14 +0900

    레이아웃 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import {
 } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
+import { Global, css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
@@ -17,6 +19,19 @@ import { setAccessToken } from './slice';
 
 import { loadItem } from './services/storage';
 
+const Layout = styled.div`
+  max-width: 768px;
+  margin: 16px auto;
+  padding: 16px;
+`;
+
+const Header = styled.header`
+  font-size: 24px;
+  h1 {
+    border-bottom: 1px solid #ddd;
+  }
+`;
+
 export default function App() {
   const dispatch = useDispatch();
 
@@ -26,12 +41,19 @@ export default function App() {
   }
 
   return (
-    <div>
-      <header>
+    <Layout className="withShadow">
+      <Global
+        styles={css`
+          .withShadow {
+            box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+          }
+        `}
+      />
+      <Header>
         <h1>
           <Link to="/">헤더 영역</Link>
         </h1>
-      </header>
+      </Header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
@@ -40,6 +62,6 @@ export default function App() {
         <Route path="/restaurants/:id" component={RestaurantPage} />
         <Route component={NotFoundPage} />
       </Switch>
-    </div>
+    </Layout>
   );
 }
