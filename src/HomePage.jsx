5c9a00c085d38e4e2017e5ba2e65b96bc4e9b086
Rok <ur_0ke@inflearn.com>
2022-01-19 23:44:46 +0900

    Home.jsx 리스트 스타일 수정

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const List = styled.ul`
+  padding: 16px;
+  list-style: none;
+  li {
+    margin-bottom: 8px;
+    border-bottom: 1px solid #ddd;
+  }
+`;
 
 export default function HomePage() {
   return (
     <div>
       <h2>Home</h2>
-      <ul>
+      <List className="withShadow">
         <li><Link to="/about">About</Link></li>
         <li><Link to="/login">Log in</Link></li>
         <li><Link to="/restaurants">Restaurants</Link></li>
         <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
+      </List>
     </div>
   );
 }
