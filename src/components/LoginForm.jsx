8fa05f8f65ce4407fd3e6810f621cf883e785dc2
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-20 19:37:27 +0900

    [refactor] 로그인 UI 최적화 - 기존 JSX 에서 TextField 컴포넌트로 대체

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,39 +1,26 @@
 import { memo } from 'react';
 
+import TextField from './TextField';
+
 const LoginForm = memo(({ fields, onChange, onSubmit }) => {
   const { email, password } = fields;
 
-  function handleChange(event) {
-    const { target: { name, value } } = event;
-    onChange({ name, value });
-  }
-
   return (
     <>
-      <div>
-        <label htmlFor="login-email">
-          E-mail
-        </label>
-        <input
-          type="email"
-          id="login-email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="login-password">
-          Password
-        </label>
-        <input
-          type="password"
-          id="login-password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </div>
+      <TextField
+        label="E-mail"
+        type="email"
+        name="email"
+        value={email}
+        onChange={onChange}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        value={password}
+        onChange={onChange}
+      />
       <button
         type="button"
         onClick={onSubmit}
--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -5,7 +5,9 @@ function TextField({
 }) {
   const id = `input-${name}`;
 
+  // THINK:
   // 이 부분은 굳이 memoized 안해도될듯
+  // 왜냐하면 해당 부분이 변경되면 handleChange 는 캐시될 필요 없음
   const handleChange = (event) => {
     const { target } = event;
     onChange({ name, value: target.value });
