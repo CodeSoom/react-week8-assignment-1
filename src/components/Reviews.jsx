2ac8d321ce0d3669b551586eb6ca320573857518
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-22 06:00:40 +0900

    [refactor] 리뷰 구조 설정

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -8,22 +8,25 @@ function Reviews({ reviews }) {
   const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
 
   return (
-    <ul>
-      {sortedReviews.map((review) => (
-        <li key={review.id}>
-          <div>
-            {review.name}
-          </div>
-          <div>
-            {review.score}
-            점
-          </div>
-          <div>
-            {review.description}
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>리뷰</h2>
+      <ul>
+        {sortedReviews.map((review) => (
+          <li key={review.id}>
+            <div>
+              {review.name}
+            </div>
+            <div>
+              {review.score}
+              점
+            </div>
+            <div>
+              {review.description}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
