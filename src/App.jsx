b9c3f213dc00ee4180a2d766058f12ccd77d1233
CheolWoong Choi <bluesky4381@naver.com>
2022-01-22 22:58:28 +0900

    앱 & 공통 스타일 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ import RestaurantPage from './pages/RestaurantPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 import { setAccessToken } from './slice';
-
 import { loadItem } from './services/storage';
+import { AppWrapper, AppHeader } from './App.style';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -26,12 +26,12 @@ export default function App() {
   }
 
   return (
-    <div>
-      <header>
+    <AppWrapper>
+      <AppHeader>
         <h1>
           <Link to="/">헤더 영역</Link>
         </h1>
-      </header>
+      </AppHeader>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
@@ -40,6 +40,6 @@ export default function App() {
         <Route path="/restaurants/:id" component={RestaurantPage} />
         <Route component={NotFoundPage} />
       </Switch>
-    </div>
+    </AppWrapper>
   );
 }
---
+++ src/App.style.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const AppWrapper = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+
+    &:visited {
+      color: #000;
+    }
+  }
+
+  li {
+    list-style-type: none;
+  }
+`;
+
+export const AppHeader = styled.header`
+  padding: 30px 0;
+
+  & > h1 {
+    font-size: 40px;
+
+    & > a {
+      &:active {
+        color: #000;
+      }
+    }
+  }
+`;
