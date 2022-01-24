656f3f6eb008a120d8c1a279f70683f289e468cb
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-24 19:04:02 +0900

    [스타일] RestaurantContainer 2개 섹션으로 나누기 DetailSection, ReviewSection

--- src/components/RestaurantContainer.jsx
+++ src/components/RestaurantContainer.jsx
@@ -2,6 +2,8 @@ import { useEffect, useCallback } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import RestaurantDetail from './RestaurantDetail';
 import ReviewForm from './ReviewForm';
 import Reviews from './Reviews';
@@ -14,6 +16,40 @@ import {
 
 import { get } from '../utils';
 
+// const SectionContainer = styled.div`
+//   display: flex;
+//   @media screen and (min-width: 412px) {
+//     flex-direction:column;
+//   }
+// `
+
+const SectionContainer = styled.div({
+  display: 'flex',
+  width: '100%',
+  flexDirection: 'row',
+  '@media screen and (max-width: 600px)': {
+    flexDirection: 'column',
+  },
+});
+
+const DetailSection = styled.div({
+  minWidth: '50%',
+  flex: '5 ',
+});
+
+const ReviewSection = styled.div({
+  background: '#009688',
+  padding: '0 0.2em',
+  minWidth: '50%',
+  flex: '5',
+});
+
+const ReviewSectionHeader = styled.div({
+  background: '#00695f',
+  padding: '0.2em',
+  fontSize: '30px',
+  fontWeight: '600',
+});
 export default function RestaurantContainer({ restaurantId }) {
   const dispatch = useDispatch();
 
@@ -40,16 +76,23 @@ export default function RestaurantContainer({ restaurantId }) {
   }
 
   return (
-    <>
-      <RestaurantDetail restaurant={restaurant} />
-      {accessToken ? (
-        <ReviewForm
-          fields={reviewFields}
-          onChange={handleChange}
-          onSubmit={handleSubmit}
-        />
-      ) : null}
-      <Reviews reviews={restaurant.reviews} />
-    </>
+    <SectionContainer>
+      <DetailSection>
+        <RestaurantDetail restaurant={restaurant} />
+      </DetailSection>
+      <ReviewSection>
+        <ReviewSectionHeader>
+          리뷰 남기기
+        </ReviewSectionHeader>
+        {accessToken ? (
+          <ReviewForm
+            fields={reviewFields}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        ) : null}
+        <Reviews reviews={restaurant.reviews} />
+      </ReviewSection>
+    </SectionContainer>
   );
 }
