1679c049bf4a0f21a1f28019c8cba4aeba3ca779
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-24 19:08:36 +0900

    [스타일] Reviews - ReviewContainer, ReviewHeader, ReviewDescription

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
+import Rating from 'react-rating';
+
+import styled from '@emotion/styled';
+
+import { Icon } from 'react-materialize';
+
 function Reviews({ reviews }) {
   if (!reviews || !reviews.length) {
     return null;
   }
+
+  const ReviewContainer = styled.div({
+    background: '#009688',
+    padding: '0.5em',
+    marginBottom: '0.5em',
+    borderRadius: 10,
+    border: 'solid 1px yellow',
+  });
+
+  const ReviewHeader = styled.div({
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '1px solid black',
+  });
+
+  const Reviewer = styled.div({
+    fontWeight: 100,
+    fontSize: '1.5em',
+    marginRight: '1em',
+  });
+
+  const ReviewScore = styled.div({
+  });
+
+  const ReviewDescription = styled.div({
+    background: '#33ab9f',
+    marginTop: '0.5em',
+    paddingTop: '0.5em',
+    paddingLeft: '1.5em',
+    borderRadius: 10,
+  });
 
   const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
 
@@ -11,16 +48,25 @@ function Reviews({ reviews }) {
     <ul>
       {sortedReviews.map((review) => (
         <li key={review.id}>
-          <div>
-            {review.name}
-          </div>
-          <div>
-            {review.score}
-            점
-          </div>
-          <div>
-            {review.description}
-          </div>
+          <ReviewContainer>
+            <ReviewHeader>
+              <Reviewer>
+                {review.name}
+              </Reviewer>
+              <ReviewScore>
+                <Rating
+                  emptySymbol={<Icon>star_border</Icon>}
+                  fullSymbol={<Icon>star</Icon>}
+                  initialRating={review.score > 5 ? 5 : review.score}
+                  readonly
+                />
+              </ReviewScore>
+            </ReviewHeader>
+
+            <ReviewDescription>
+              {review.description}
+            </ReviewDescription>
+          </ReviewContainer>
         </li>
       ))}
     </ul>
