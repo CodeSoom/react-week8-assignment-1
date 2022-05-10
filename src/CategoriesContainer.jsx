2ecee3ef9ea85290fe3b3f83982576d0fceac21c
이찬호 <0407chan@naver.com>
2022-05-10 23:26:45 +0900

    emotion 스타일 간단 적용

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -7,6 +7,9 @@ import {
 
 import { get } from './utils';
 
+import MenuList from './MenuList';
+import MenuItem from './MenuItem';
+
 export default function CategoriesContainer() {
   const dispatch = useDispatch();
 
@@ -19,9 +22,9 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <ul>
+    <MenuList>
       {categories.map((category) => (
-        <li key={category.id}>
+        <MenuItem key={category.id}>
           <button
             type="button"
             onClick={() => handleClick(category.id)}
@@ -33,8 +36,8 @@ export default function CategoriesContainer() {
               </>
             ) : null}
           </button>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
