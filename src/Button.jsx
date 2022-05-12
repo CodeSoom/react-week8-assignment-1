794ab0a3b26c3bcd97b24a218590901c9d7a2aa6
0hu <dladudgn123@naver.com>
2022-05-12 10:25:40 +0900

    Button , Form 컴포넌트 분리

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.5em 1em;
+`;
+
+export default Button;
