fc6dfa8ab7dc9816d958dfc93f367a9e1eb5ca9c
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-21 08:58:02 +0900

    index.jsx의 store 경로 수정
    index.jsx의 경로가 잘못되어 있어서 이를 수정했습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,18 @@
 import ReactDOM from 'react-dom';
 
-import {
-  BrowserRouter,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
 import App from './App';
 
-import store from './store';
+import store from './redux/slice';
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  ),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('app'),
 );
