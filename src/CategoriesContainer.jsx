6524cd25b8c3bbf970ae71e4d30d16f814815968
Ga-Young Song <dolphin2440@naver.com>
2022-06-21 22:11:32 +0900

    첫번째 강의

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -4,6 +4,8 @@ import {
   selectCategory,
   loadRestaurants,
 } from './actions';
+import MenuItem from './MenuItem';
+import MenuList from './MenuList';
 
 import { get } from './utils';
 
@@ -19,9 +21,12 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <ul>
+    <MenuList>
       {categories.map((category) => (
-        <li key={category.id}>
+        <MenuItem
+          key={category.id}
+          active={selectedCategory && category.id === selectedCategory.id}
+        >
           <button
             type="button"
             onClick={() => handleClick(category.id)}
@@ -33,8 +38,8 @@ export default function CategoriesContainer() {
               </>
             ) : null}
           </button>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
