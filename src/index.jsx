791abf93dfa2e54ccdab84b788c532ce28c7ae10
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-22 08:42:47 +0900

    index.jsx에서 store의 경로가 slice에서 store로 수정
    기존 index.jsx의 store를 store가 아닌 slice로 경로가 설정되어 있어서 에러가 발생하였기에
    이를 수정했습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 
-import store from './redux/slice';
+import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
