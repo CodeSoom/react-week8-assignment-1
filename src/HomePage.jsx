927eda8fa3fca14464bf0f9d8f66ae9912364f0e
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-21 09:45:34 +0900

    HomePage Components Styled Add

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,15 +1,50 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+const Title = styled.h2({
+  margin: '0',
+  padding: '.4em 0',
+  fontSize: '2rem',
+});
+
+const List = styled.ul({
+  display: 'flex',
+  margin: 0,
+  padding: 0,
+  listStyle: 'none',
+});
+
+const Item = styled.li({
+  marginRight: '1rem',
+  '& a': {
+    textDecoration: 'none',
+    color: '#333',
+    '&:hover': {
+      fontWeight: 'bold',
+      color: '#000',
+    },
+  },
+});
+
 export default function HomePage() {
   return (
     <div>
-      <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
+      <Title>Home</Title>
+      <List>
+        <Item>
+          <Link to="/about">About</Link>
+        </Item>
+        <Item>
+          <Link to="/login">Log in</Link>
+        </Item>
+        <Item>
+          <Link to="/restaurants">Restaurants</Link>
+        </Item>
+        <Item>
+          <Link to="/xxx">멸망의 길</Link>
+        </Item>
+      </List>
     </div>
   );
 }
