2eaa0e6f17ff2fd1cd2dbf99a4fcfa70abdaa7d6
rukeni <rukeni@naver.com>
2022-06-23 14:35:23 +0900

    강의 반영

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   selectCategory,
   loadRestaurants,
-} from './actions';
+} from './slice';
+
+import MenuItem from './MenuItem';
+import MenuList from './MenuList';
 
 import { get } from './utils';
 
@@ -19,9 +22,12 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <ul>
+    <MenuList>
       {categories.map((category) => (
-        <li key={category.id}>
+        <MenuItem
+          key={category.id}
+          active={selectedCategory && category.id === selectedCategory.id}
+        >
           <button
             type="button"
             onClick={() => handleClick(category.id)}
@@ -33,8 +39,8 @@ export default function CategoriesContainer() {
               </>
             ) : null}
           </button>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
