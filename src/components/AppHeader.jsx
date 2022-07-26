5ef370a8e7e7d0ba6539cf90974703ade5aba82c
kyh196201 <kyh196201@gmail.com>
2022-07-26 20:27:18 +0900

    AppHeader 컴포넌트 스타일링

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,13 +1,22 @@
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 
 import { Link } from 'react-router-dom';
 
+const Header = styled.header({
+  backgroundColor: '#898AA6',
+  padding: '1rem 3rem',
+});
+
+const Logo = styled.h1({
+  fontSize: '3rem',
+});
+
 export default function AppHeader() {
   return (
-    <header>
-      <h1>
+    <Header>
+      <Logo>
         <Link to="/">마이 레스토랑</Link>
-      </h1>
-    </header>
+      </Logo>
+    </Header>
   );
 }
