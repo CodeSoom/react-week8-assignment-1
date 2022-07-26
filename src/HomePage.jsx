81923f8eb224b909370c06040c75452b76c361ff
kyh196201 <kyh196201@gmail.com>
2022-07-26 20:40:26 +0900

    HomePage 리팩토링
    - HomePage에 타이틀, NavBar가 렌더링되었는지 확인하는 테스트 코드를 작성했습니다.
    - HomePage 내부 ul 영역을 NavBar 컴포넌트로 대체했습니다.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,15 +1,11 @@
-import { Link } from 'react-router-dom';
+import NavBar from './components/home/NavBar';
 
 export default function HomePage() {
   return (
     <div>
       <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
+
+      <NavBar />
     </div>
   );
 }
--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,12 +2,30 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
+import { ROUTES } from './components/home/NavBar';
+
 import HomePage from './HomePage';
 
-test('HomePage', () => {
-  render((
+describe('<HomePage />', () => {
+  const renderHomePage = () => render((
     <MemoryRouter>
       <HomePage />
     </MemoryRouter>
   ));
+
+  it('renders title', () => {
+    const { container } = renderHomePage();
+
+    expect(container).toHaveTextContent('Home');
+  });
+
+  it('renders navigations', () => {
+    const { getByText } = renderHomePage();
+
+    ROUTES.forEach(({ name }) => {
+      const navigation = getByText(name);
+
+      expect(navigation).toBeInTheDocument();
+    });
+  });
 });
