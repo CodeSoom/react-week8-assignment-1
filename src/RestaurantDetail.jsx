ed336a16de2209e684c752b9c86daec7341df069
minsuk <alstjr0183@naver.com>
2022-07-27 01:14:38 +0900

    feat: RestaurantDetail - css 수정

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -1,18 +1,52 @@
+import styled from '@emotion/styled';
 import MenuItems from './MenuItems';
 
 export default function RestaurantDetail({ restaurant }) {
   const { name, address, menuItems } = restaurant;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>
-        주소:
-        {' '}
-        {address}
-      </p>
-      <h3>메뉴</h3>
-      <MenuItems menuItems={menuItems} />
-    </div>
+    <Container>
+      <Wrap>
+        {name}
+        <p className="adress">
+          주소:
+          {address}
+        </p>
+
+        <p className="menu">메뉴</p>
+        <MenuItems menuItems={menuItems} />
+      </Wrap>
+
+    </Container>
   );
 }
+
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Wrap = styled.h2({
+  borderRadius: '10px',
+  width: '90%',
+  backgroundColor: 'pink',
+  padding: '10px',
+  textAlign: 'center',
+  color: 'white',
+
+  '.adress': {
+    fontSize: '15px',
+    textAlign: 'right',
+    margin: '0',
+    marginBottom: '10px',
+  },
+
+  '.menu': {
+    background: 'white',
+    color: 'skyblue',
+    textAlign: 'left',
+    margin: '0',
+    padding: '10px',
+  },
+});
