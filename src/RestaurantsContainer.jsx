0c1333c0647fa3052e7f388472d9fbc75921585f
minsuk <alstjr0183@naver.com>
2022-07-27 00:52:37 +0900

    fix: RestaurantsContainer - css 수정

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { useSelector } from 'react-redux';
 
 import { get } from './utils';
@@ -13,14 +14,34 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
   }
 
   return (
-    <ul>
+    <MenuList>
       {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
+        <MenuItem key={restaurant.id}>
           <a href="/restaurants/1" onClick={handleClick(restaurant)}>
             {restaurant.name}
           </a>
-        </li>
+        </MenuItem>
       ))}
-    </ul>
+    </MenuList>
   );
 }
+
+const MenuList = styled.ul({
+  display: 'flex',
+  flexDirection: 'column',
+  borderTop: '1px solid skyblue',
+  padding: '30px 0',
+  margin: '0',
+  listStyle: 'none',
+});
+
+const MenuItem = styled.li(() => ({
+  borderRadius: '10px',
+  background: 'skyblue',
+  marginBottom: '10px',
+  padding: '10px',
+  '& a': {
+    textDecoration: 'none',
+    color: 'white',
+  },
+}));
