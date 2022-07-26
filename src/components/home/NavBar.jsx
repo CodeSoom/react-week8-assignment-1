70708937f1c4ecc796ca0e9e114a5a06bbca7829
kyh196201 <kyh196201@gmail.com>
2022-07-26 20:47:18 +0900

    NavBar 컴포넌트 스타일링

--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
 export const ROUTES = [
   {
     name: 'About',
@@ -19,16 +21,43 @@ export const ROUTES = [
   },
 ];
 
+const Navigations = styled.ul({
+  display: 'flex',
+  margin: '0',
+  padding: '1rem',
+});
+
+const Navigation = styled.li({
+  marginRight: '1.5rem',
+
+  '&:last-child': {
+    marginRight: '0',
+  },
+
+  '& a': {
+    fontSize: '2rem',
+    fontWeight: 'bolder',
+    color: '#495C83',
+    transition: 'all 0.5s',
+
+    '&:hover': {
+      opacity: '0.8',
+    },
+  },
+});
+
 export default function NavBar() {
   return (
-    <ul>
-      {
-        ROUTES.map(({ name, path }) => (
-          <li key={name}>
-            <Link to={path}>{name}</Link>
-          </li>
-        ))
-      }
-    </ul>
+    <nav>
+      <Navigations>
+        {
+          ROUTES.map(({ name, path }) => (
+            <Navigation key={name}>
+              <Link to={path}>{name}</Link>
+            </Navigation>
+          ))
+        }
+      </Navigations>
+    </nav>
   );
 }
