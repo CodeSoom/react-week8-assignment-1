531a8580b19f79f97b006c506b345b89391479cf
seungwoo Kim <kyh196201@gmail.com>
2022-07-29 19:10:32 +0900

    appSlice 제거

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import appReducer from './appSlice';
 import loginReducer from '../features/login/loginSlice';
 
 const rootReducer = {
-  app: appReducer,
   login: loginReducer,
 };
 
