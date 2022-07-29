fc96aefd932bad05869ffc2206695978567b21eb
seungwoo Kim <kyh196201@gmail.com>
2022-07-29 20:27:20 +0900

    rootReducer에 regionsReducer, categoriesReducer 추가

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import loginReducer from '../features/login/loginSlice';
+import regionsReducer from '../features/regions/regionsSlice';
+import categoriesReducer from '../features/categories/categoriesSlice';
 
 const rootReducer = {
   login: loginReducer,
+  regions: regionsReducer,
+  categories: categoriesReducer,
 };
 
 const store = configureStore({
