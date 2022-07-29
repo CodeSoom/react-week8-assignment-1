458f0a80ef8621f5d79a8cb2a5f1420bddad6e91
Woo Dy <kjop7419@naver.com>
2022-07-29 20:29:35 +0900

    refactor(MenuItem.test)

--- src/styles/MenuItem.test.jsx
+++ src/styles/MenuItem.test.jsx
@@ -3,13 +3,15 @@ import { render } from '@testing-library/react';
 import MenuItem from './MenuItem';
 
 describe('MenuItem', () => {
-  it('active is true', () => {
-    const { getByText } = render((
-      <MenuItem active>
-        <p>안녕</p>
-      </MenuItem>
-    ));
+  context('active is true', () => {
+    it('background is #EEEEEE', () => {
+      const { getByText } = render((
+        <MenuItem active>
+          <p>안녕</p>
+        </MenuItem>
+      ));
 
-    expect(getByText('안녕').parentElement).toHaveStyle('background: rgb 238, 238, 238');
+      expect(getByText('안녕').parentElement).toHaveStyle('background: rgb 238, 238, 238');
+    });
   });
 });
