3a0704e2c348ec9ac15ac76a0ca748e20954a762
2scent <ad7549@gmail.com>
2022-07-29 20:37:37 +0900

    Decorate Reviews

--- src/pages/RestaurantPage/Reviews/Reviews.jsx
+++ src/pages/RestaurantPage/Reviews/Reviews.jsx
@@ -1,3 +1,25 @@
+import styled from '@emotion/styled';
+
+const List = styled.ul({
+  margin: 0,
+  padding: '1rem',
+  listStyle: 'none',
+});
+
+const Review = styled.li({
+  marginBottom: '1rem',
+  padding: '.5rem',
+  border: '1px solid #000000',
+});
+
+const Name = styled.span({
+  fontWeight: 'bold',
+});
+
+const Description = styled.div({
+  marginTop: '.7rem',
+});
+
 export default function Reviews({ reviews }) {
   if (!reviews || !reviews.length) {
     return null;
@@ -6,21 +28,22 @@ export default function Reviews({ reviews }) {
   const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
 
   return (
-    <ul>
+    <List>
       {sortedReviews.map((review) => (
-        <li key={review.id}>
-          <div>
+        <Review key={review.id}>
+          <Name>
             {review.name}
-          </div>
-          <div>
+          </Name>
+          {' - '}
+          <span>
             {review.score}
             점
-          </div>
-          <div>
+          </span>
+          <Description>
             {review.description}
-          </div>
-        </li>
+          </Description>
+        </Review>
       ))}
-    </ul>
+    </List>
   );
 }
