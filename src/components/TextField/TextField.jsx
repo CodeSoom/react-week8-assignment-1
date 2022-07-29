850825753c0a9f7438193e0f839b6b568efcc20f
2scent <ad7549@gmail.com>
2022-07-29 19:48:30 +0900

    Decorate text field

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,3 +1,21 @@
+import styled from '@emotion/styled';
+
+const Label = styled.label({
+  display: 'block',
+  marginBottom: '.2rem',
+  fontWeight: 500,
+});
+
+const Input = styled.input({
+  padding: '.4rem',
+  border: '.5px solid #AAAAAA',
+  borderRadius: '.375rem',
+  '&:focus': {
+    outline: 'none',
+    boxShadow: '0 0 0 1px #B2EBF4',
+  },
+});
+
 export default function TextField({
   label, type = 'text', name, value, onChange,
 }) {
@@ -10,10 +28,10 @@ export default function TextField({
 
   return (
     <div>
-      <label htmlFor={id}>
+      <Label htmlFor={id}>
         {label}
-      </label>
-      <input
+      </Label>
+      <Input
         type={type}
         id={id}
         name={name}
