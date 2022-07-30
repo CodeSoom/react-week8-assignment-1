29098f3245e004d52faa12f047bc8c88b0d33628
seungwoo Kim <kyh196201@gmail.com>
2022-07-30 14:35:30 +0900

    restaurants fixture 수정
    - 레스토랑 목록 렌더링 테스트를 위해서 항목을 추가했습니다.

--- fixtures/restaurants.js
+++ fixtures/restaurants.js
@@ -1,6 +1,15 @@
 const restaurants = [
   {
-    id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
+    id: 1,
+    name: '김밥제국',
+    category: '분식',
+    address: '서울시 강남구 역삼동',
+  },
+  {
+    id: 1,
+    category: '중식',
+    name: '양천주가',
+    address: '서울 강남구',
   },
 ];
 
