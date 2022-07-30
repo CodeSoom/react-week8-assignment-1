852521c8765b1e35af89d6d35f6c6a5260f02bee
seungwoo Kim <kyh196201@gmail.com>
2022-07-30 15:56:10 +0900

    rootReducer에 reviewReducer 추가

--- src/app/store.js
+++ src/app/store.js
@@ -5,6 +5,7 @@ import regionsReducer from '../features/regions/regionsSlice';
 import categoriesReducer from '../features/categories/categoriesSlice';
 import restaurantsReducer from '../features/restaurants/restaurantsSlice';
 import restaurantReducer from '../features/restaurant/restaurantSlice';
+import reviewReducer from '../features/review/reviewSlice';
 
 const rootReducer = {
   login: loginReducer,
@@ -12,6 +13,7 @@ const rootReducer = {
   categories: categoriesReducer,
   restaurants: restaurantsReducer,
   restaurant: restaurantReducer,
+  review: reviewReducer,
 };
 
 const store = configureStore({
