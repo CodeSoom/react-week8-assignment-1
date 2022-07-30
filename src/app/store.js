5a2fcda2e057d195b40418c84f397a1df617d3d5
seungwoo Kim <kyh196201@gmail.com>
2022-07-30 15:27:46 +0900

    rootReducer에 restaurantReducer 추가

--- src/app/store.js
+++ src/app/store.js
@@ -4,12 +4,14 @@ import loginReducer from '../features/login/loginSlice';
 import regionsReducer from '../features/regions/regionsSlice';
 import categoriesReducer from '../features/categories/categoriesSlice';
 import restaurantsReducer from '../features/restaurants/restaurantsSlice';
+import restaurantReducer from '../features/restaurant/restaurantSlice';
 
 const rootReducer = {
   login: loginReducer,
   regions: regionsReducer,
   categories: categoriesReducer,
   restaurants: restaurantsReducer,
+  restaurant: restaurantReducer,
 };
 
 const store = configureStore({
