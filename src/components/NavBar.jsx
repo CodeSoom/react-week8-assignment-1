8da036347c1bc965ba77733b9720e75b8ce55571
kyh196201 <kyh196201@gmail.com>
2022-08-01 20:33:26 +0900

    NavBar 컴포넌트 스타일 수정
    - 현재 경로와 일치하는 경우에 Navigation  컴포넌트에 특정 스타일을 추가했습니다.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 import styled from '@emotion/styled';
 
@@ -9,33 +9,41 @@ const Navigations = styled.ul({
   margin: '0',
 });
 
-const Navigation = styled.li({
-  marginRight: '1.5rem',
+const Navigation = styled.li(({ active }) => ({
+  marginRight: '2rem',
 
   '&:last-child': {
     marginRight: '0',
   },
 
   '& a': {
-    fontSize: '2rem',
+    fontSize: '1.6rem',
     fontWeight: 'bolder',
-    color: '#495C83',
     transition: 'all 0.5s',
 
     '&:hover': {
-      opacity: '0.8',
+      opacity: '1',
       textDecoration: 'underline',
     },
+
+    color: active ? '#fa5252' : '#000000',
+    opacity: active ? '1' : '0.6',
+    textDecoration: active ? 'underline' : 'none',
   },
-});
+}));
 
 export default function NavBar() {
+  const isActive = (path) => useRouteMatch({
+    path,
+    exact: true,
+  });
+
   return (
     <nav>
       <Navigations>
         {
           ROUTES.map(({ name, path }) => (
-            <Navigation key={name}>
+            <Navigation key={name} active={isActive(path)}>
               <Link to={path}>{name}</Link>
             </Navigation>
           ))
