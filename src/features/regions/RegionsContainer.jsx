81c980d78deaf53ba7f046853ecc6af222141e2d
kyh196201 <kyh196201@gmail.com>
2022-08-01 21:27:35 +0900

    RegionsContainer 스타일 수정

--- src/features/regions/RegionsContainer.jsx
+++ src/features/regions/RegionsContainer.jsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+
 import { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,6 +13,33 @@ import {
 
 import { loadRestaurants } from '../restaurants/restaurantsSlice';
 
+const Title = styled.h3({
+  margin: '0 0 1rem 0',
+  fontSize: '2rem',
+});
+
+const List = styled.ul({
+  display: 'flex',
+});
+
+const Item = styled.li({
+  marginRight: '1.5rem',
+
+  '&:last-child': {
+    marginRight: 0,
+  },
+});
+
+const MenuButton = styled.button(({ selected }) => ({
+  padding: '1rem 1.5rem',
+  border: '2px solid #495057',
+  borderRadius: '0.5rem',
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  color: selected ? '#ffffff' : '#495057',
+  backgroundColor: selected ? '#495057' : '#ffffff',
+}));
+
 export default function RegionsContainer() {
   const dispatch = useDispatch();
 
@@ -21,10 +50,18 @@ export default function RegionsContainer() {
   const regions = useSelector(regionsSelector);
   const selectedRegion = useSelector(selectedRegionSelector);
 
-  function handleClick(regionId) {
+  const handleClick = (regionId) => {
     dispatch(selectRegion({ regionId }));
     dispatch(loadRestaurants());
-  }
+  };
+
+  const isSelected = (regionId) => {
+    if (!selectedRegion) {
+      return false;
+    }
+
+    return selectedRegion.id === regionId;
+  };
 
   if (!regions?.length) {
     return (
@@ -35,22 +72,26 @@ export default function RegionsContainer() {
   }
 
   return (
-    <ul>
-      {regions.map((region) => (
-        <li key={region.id}>
-          <button
-            type="button"
-            onClick={() => handleClick(region.id)}
-          >
-            {region.name}
-            {selectedRegion ? (
-              <>
-                {region.id === selectedRegion.id ? '(V)' : null}
-              </>
-            ) : null}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Title>지역 목록</Title>
+      <List>
+        {regions.map((region) => (
+          <Item key={region.id}>
+            <MenuButton
+              selected={isSelected(region.id)}
+              type="button"
+              onClick={() => handleClick(region.id)}
+            >
+              {region.name}
+              {selectedRegion ? (
+                <>
+                  {isSelected(region.id) ? '(V)' : null}
+                </>
+              ) : null}
+            </MenuButton>
+          </Item>
+        ))}
+      </List>
+    </>
   );
 }
