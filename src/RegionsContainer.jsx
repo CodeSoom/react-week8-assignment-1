a05aea36171e1fbdfe33f34a9ffa78a91b42c32b
0-Chan <kyc4421194@gmail.com>
2022-09-29 15:37:27 +0900

    Add useCallback in containers

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import {
@@ -13,10 +14,10 @@ export default function RegionsContainer() {
   const regions = useSelector(get('regions'));
   const selectedRegion = useSelector(get('selectedRegion'));
 
-  function handleClick(regionId) {
+  const handleClick = useCallback((regionId) => {
     dispatch(selectRegion(regionId));
     dispatch(loadRestaurants());
-  }
+  }, [dispatch]);
 
   return (
     <ul>
