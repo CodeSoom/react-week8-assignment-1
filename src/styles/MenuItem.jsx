fb73b26823312514751b7661d2f12e75f74980bb
ChoJinmok <jjmok1127@gmail.com>
2022-10-01 22:15:48 +0900

    Modify MenuItem style

--- src/styles/MenuItem.jsx
+++ src/styles/MenuItem.jsx
@@ -1,16 +1,18 @@
 import styled from '@emotion/styled';
 
-const MenuItem = styled.li(({ active }) => ({
+const MenuItem = styled.li(() => ({
   '&:not(:last-child)': {
     marginRight: '1em',
   },
+
   '& button': {
     padding: '.4em 1em',
     border: '1px solid #CCC',
-    backgroundColor: active ? '#EEE' : 'transparent',
+    backgroundColor: '#EEE',
     color: '#333',
     textDecoration: 'none',
     cursor: 'pointer',
+
     '&:hover': {
       color: '#000',
       fontWeight: 'bold',
