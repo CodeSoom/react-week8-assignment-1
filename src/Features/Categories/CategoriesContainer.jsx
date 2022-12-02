db0c598d9bd54192629a36ebb9ef699b95af2fed
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-02 20:50:28 +0900

    CategoriesContainer 컴포넌트 스타일링
    fontawesome을 활용하여 스타일링을 하였습니다

--- src/Features/Categories/CategoriesContainer.jsx
+++ src/Features/Categories/CategoriesContainer.jsx
@@ -1,11 +1,43 @@
 import { useDispatch, useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBowlFood,
+  faPepperHot,
+  faFishFins,
+  faPizzaSlice,
+  faCookie,
+  faDrumstickBite,
+  faCandyCane,
+} from '@fortawesome/free-solid-svg-icons';
 import {
   selectCategory,
   loadRestaurants,
 } from '../../slice';
 
 import { get } from '../../utils';
+
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+`;
+
+const Item = styled.li`
+  width: 180px;
+  height: 65px;
+  border: 1px solid #000;
+  text-align: center;
+  line-height: 65px;
+
+  & button {
+    width: 180px;
+    height: 65px;
+    text-align: center;
+  }
+`;
 
 export default function CategoriesContainer() {
   const dispatch = useDispatch();
@@ -18,23 +50,36 @@ export default function CategoriesContainer() {
     dispatch(loadRestaurants());
   }
 
+  const categoryLogo = {
+    한식: faBowlFood,
+    중식: faPepperHot,
+    일식: faFishFins,
+    양식: faPizzaSlice,
+    분식: faCookie,
+    과자: faCandyCane,
+    치킨: faDrumstickBite,
+  };
   return (
-    <ul>
-      {categories.map((category) => (
-        <li key={category.id}>
-          <button
-            type="button"
-            onClick={() => handleClick(category.id)}
-          >
-            {category.name}
-            {selectedCategory ? (
-              <>
-                {category.id === selectedCategory.id ? '(V)' : null}
-              </>
-            ) : null}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <p>카테고리</p>
+      <List>
+        {categories.map((category) => (
+          <Item key={category.id}>
+            <button
+              type="button"
+              onClick={() => handleClick(category.id)}
+            >
+              <FontAwesomeIcon icon={categoryLogo[category.name]} />
+              {category.name}
+              {selectedCategory ? (
+                <>
+                  {category.id === selectedCategory.id ? '(V)' : null}
+                </>
+              ) : null}
+            </button>
+          </Item>
+        ))}
+      </List>
+    </>
   );
 }
