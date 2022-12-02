4657ed8f65ca7efa9222ecc2c0cc2dc020b31a08
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-02 20:42:56 +0900

    App 컴포넌트에서서 Header 분리 및 스타일링 reset 지정
    Global을 이용하여 초기화를 지정하였으며며 Header를 컴포넌트로 분리하였습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import {
   Switch,
   Route,
-  Link,
 } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
 
+import { Global } from '@emotion/react';
 import HomePage from './Features/Home/HomePage';
 import AboutPage from './Features/About/AboutPage';
 import LoginPage from './Features/Login/LoginPage';
@@ -16,6 +16,8 @@ import NotFoundPage from './Features/Not-found/NotFoundPage';
 import { setAccessToken } from './slice';
 
 import { loadItem } from './services/storage';
+import Header from './Components/Header';
+import reset from './styles/reset';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -27,11 +29,8 @@ export default function App() {
 
   return (
     <div>
-      <header>
-        <h1>
-          <Link to="/">헤더 영역</Link>
-        </h1>
-      </header>
+      <Global styles={reset} />
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
