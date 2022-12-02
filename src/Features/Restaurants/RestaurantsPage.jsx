555d841b3aa5b1cb20b26e9c59a8f398f0fc4a9d
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-02 20:49:11 +0900

    RestaurantsPage 컴포넌트 스타일링
    서브배너 추가 및 전체 컨테이너를 추가하여 스타일링을 하였습니다

--- src/Features/Restaurants/RestaurantsPage.jsx
+++ src/Features/Restaurants/RestaurantsPage.jsx
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import RegionsContainer from '../Regions/RegionsContainer';
 import CategoriesContainer from '../Categories/CategoriesContainer';
 import RestaurantsContainer from './RestaurantsContainer';
@@ -11,6 +13,66 @@ import RestaurantsContainer from './RestaurantsContainer';
 import {
   loadInitialData,
 } from '../../slice';
+
+const SubBanner = styled.div({
+  // width: '100vw',
+  height: '400px',
+  background: 'url(../../../img/sub-bg1.png)',
+  backgroundSize: 'cover',
+  position: 'relative',
+
+});
+
+const SubDescription = styled.div({
+  width: '90%',
+  margin: '0 auto',
+  position: 'absolute',
+  left: '8rem',
+  bottom: '6rem',
+
+  '& h1': {
+    color: '#ffffff',
+    fontSize: '9rem',
+    marginRight: '5rem',
+    fontWeight: '800',
+    position: 'relative',
+
+    '&:after': {
+      position: 'absolute',
+      top: 0,
+      left: '60rem',
+      display: 'block',
+      content: '""',
+      width: '15px',
+      height: '15px',
+      backgroundColor: '#DB0816',
+      borderRadius: '50px',
+    },
+  },
+
+  '& p': {
+    color: '#ffffff',
+    fontWeight: '300',
+    fontSize: '2.5rem',
+  },
+});
+
+const Container = styled.div({
+  width: '90%',
+  margin: '0 auto',
+  paddingTop: '7rem',
+
+  '& h2': {
+    fontSize: '3.5rem',
+    marginBottom: '2rem',
+  },
+
+  '& p': {
+    fontSize: '2rem',
+    lineHeight: '1.5',
+    marginBottom: '3rem',
+  },
+});
 
 export default function RestaurantsPage() {
   const history = useHistory();
@@ -28,9 +90,17 @@ export default function RestaurantsPage() {
 
   return (
     <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer onClickRestaurant={handleClickRestaurant} />
+      <SubBanner>
+        <SubDescription>
+          <p>맛있는 발견의 즐거움을 당신에게 드립니다</p>
+          <h1>Restaurants</h1>
+        </SubDescription>
+      </SubBanner>
+      <Container>
+        <RegionsContainer />
+        <CategoriesContainer />
+        <RestaurantsContainer onClickRestaurant={handleClickRestaurant} />
+      </Container>
     </div>
   );
 }
