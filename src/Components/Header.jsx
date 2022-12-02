2711832707e424e023139aa7d58a2b431f52d8d8
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-02 20:44:01 +0900

    Header 컴포넌트 생성 및 스타일링
    App 컴포넌트에서 헤더 영역을 분리하여 스타일링하였습니다

---
+++ src/Components/Header.jsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+import { Link } from 'react-router-dom';
+
+const Nav = styled.header({
+  width: '90%',
+  margin: '0 auto',
+  padding: '2rem 0',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const Logo = styled.div({
+
+});
+
+const Navigation = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  listStyle: 'none',
+});
+
+const NavItem = styled.li({
+  padding: '0 5rem',
+  fontSize: '1.5rem',
+  fontWeight: '500',
+});
+
+export default function Header() {
+  return (
+    <Nav>
+      <Logo>
+        <Link to="/">
+          <img src="../../img/logo.png" alt="eatGo-logo" />
+        </Link>
+      </Logo>
+      <Navigation>
+        <NavItem><Link to="/about">About</Link></NavItem>
+        <NavItem><Link to="/restaurants">Restaurants</Link></NavItem>
+        <NavItem><Link to="/login">Log in</Link></NavItem>
+      </Navigation>
+    </Nav>
+  );
+}
