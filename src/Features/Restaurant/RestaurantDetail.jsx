efcfa572ca5a59e537f89871020ddedf3b1914dc
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:20:19 +0900

    RestaurantDetail 컴포넌트 스타일링
    emotion을 활용하여 전체적인 스타일링을 완료하였습니다

--- src/Features/Restaurant/RestaurantDetail.jsx
+++ src/Features/Restaurant/RestaurantDetail.jsx
@@ -1,18 +1,47 @@
+import styled from '@emotion/styled';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faLocationDot, faUtensils,
+} from '@fortawesome/free-solid-svg-icons';
+
 import MenuItems from './MenuItems';
+import MenuTitle from '../../styles/MenuTitle';
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const MenuInfo = styled.div`
+  display: flex;
+  margin: 1rem 0;
+  & p {
+    margin-left: 1.5rem;
+  }
+`;
+
+const Address = styled.div`
+  margin-left: 3rem;
+`;
 
 export default function RestaurantDetail({ restaurant }) {
   const { name, address, menuItems } = restaurant;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>
-        주소:
-        {' '}
-        {address}
-      </p>
-      <h3>메뉴</h3>
-      <MenuItems menuItems={menuItems} />
-    </div>
+    <Container>
+      <MenuTitle>{name}</MenuTitle>
+      <MenuInfo>
+        <FontAwesomeIcon icon={faLocationDot} />
+        <p>주소</p>
+        <Address>{address}</Address>
+      </MenuInfo>
+      <MenuInfo>
+        <FontAwesomeIcon icon={faUtensils} />
+        <p>메뉴</p>
+        <MenuItems menuItems={menuItems} />
+      </MenuInfo>
+    </Container>
   );
 }
