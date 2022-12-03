9c029da15ad35202e232ac3ba247514a889dd8b2
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:15:27 +0900

    List 스타일링 파일 분리 및 정리
    반복되는 List 스타일링을 styles로 분리하여 정리하였습니다

---
+++ src/styles/MenuList.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const MenuList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+  margin-bottom: 7rem;
+`;
+
+export default MenuList;
