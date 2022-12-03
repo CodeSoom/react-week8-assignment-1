de5404b24a32e1eeac4ad84951cc4df170950837
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:22:52 +0900

    LogoutForm 컴포넌트 스타일링
    로그인을 누를 시 로그아웃 폼으로 가게 되는 화면을 스타일링하였습니다

--- src/Features/Login/LogoutForm.jsx
+++ src/Features/Login/LogoutForm.jsx
@@ -1,12 +1,31 @@
+import styled from '@emotion/styled';
+
+const Greeting = styled.div`
+  font-size: 2rem;
+  line-height: 1.5;
+  margin-bottom: 10rem;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 45px;
+  color : #ffffff;
+  background: #DB0816;
+`;
+
 export default function LogoutForm({ onClick }) {
   return (
     <>
-      <button
+      <Greeting>
+        <p>반갑습니다</p>
+        <p>맛집을 찾으러 가볼까요?</p>
+      </Greeting>
+      <Button
         type="button"
         onClick={onClick}
       >
         Log out
-      </button>
+      </Button>
     </>
   );
 }
