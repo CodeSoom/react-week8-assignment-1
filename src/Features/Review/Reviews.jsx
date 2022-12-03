749eae06cefff39724c743817cfd572e3df71a1c
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:23:37 +0900

    Reviews 컴포넌트 스타일링 및 별점 기능 추가
    입력한 숫자가 별로 환산되어 repeat을 이용하여 출력할 수 있도록 하였습니다

--- src/Features/Review/Reviews.jsx
+++ src/Features/Review/Reviews.jsx
@@ -1,5 +1,48 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import MenuTitle from '../../styles/MenuTitle';
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const ReviewItem = styled.li`
+  display: flex;
+  margin: 1.5rem 0;
+  border-bottom: 1px solid #e0e0e0;
+  padding: 1.5rem;
+
+  &:last-child {
+    border: none;
+  }
+`;
+
+const Reviewer = styled.div`
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  & div {
+    text-align: center;
+    margin-top: 1rem;
+  }
+`;
+
+const ReviewComment = styled.div`
+  width: 95%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin-left: 2rem;
+`;
+
 function Reviews({ reviews }) {
   if (!reviews || !reviews.length) {
     return null;
@@ -7,23 +50,45 @@ function Reviews({ reviews }) {
 
   const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
 
+  function changeStar(number) {
+    if (number < 1) return '⭐️'.repeat(1);
+
+    if (number > 5) return '⭐️'.repeat(5);
+
+    return '⭐️'.repeat(number);
+  }
+
   return (
-    <ul>
-      {sortedReviews.map((review) => (
-        <li key={review.id}>
-          <div>
-            {review.name}
-          </div>
-          <div>
-            {review.score}
-            점
-          </div>
-          <div>
-            {review.description}
-          </div>
-        </li>
-      ))}
-    </ul>
+
+    <Container>
+      <MenuTitle>
+        리뷰
+        {' '}
+        (
+        {sortedReviews.length}
+        )
+      </MenuTitle>
+      <ul>
+        {sortedReviews.map((review) => (
+          <ReviewItem key={review.id}>
+            <Reviewer>
+              <FontAwesomeIcon icon={faUserCircle} size="2x" />
+              <div>
+                {review.name}
+              </div>
+            </Reviewer>
+            <ReviewComment>
+              <div>
+                {changeStar(review.score)}
+              </div>
+              <div>
+                {review.description}
+              </div>
+            </ReviewComment>
+          </ReviewItem>
+        ))}
+      </ul>
+    </Container>
   );
 }
 
