033455f58629c45e71a76e1d492adb58acba22bb
sejineeee <sejinee0918@gmail.com>
2022-12-03 21:10:44 +0900

    HomePage에 nav 스타일 구현, SlideContainer 컴포넌트 호출

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,43 @@
 import { Link } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
+import SlideContainer from '../../components/Slide/SlideContainer';
+
+const Container = styled.div({
+  position: 'relative',
+});
+
+const List = styled.ul({
+  display: 'flex',
+  justifyContent: 'space-between',
+  position: 'absolute',
+  top: '-60px',
+  right: 0,
+  width: '40%',
+  height: '100px',
+});
+
+const Item = styled.li({
+  fontFamily: 'Roboto',
+  fontSize: '20px',
+  '& a': {
+    display: 'block',
+    padding: '8px',
+    color: '#000000',
+  },
+});
+
 export default function HomePage() {
   return (
-    <div>
-      <h2>Home</h2>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Log in</Link></li>
-        <li><Link to="/restaurants">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
-      </ul>
-    </div>
+    <Container>
+      <List>
+        <Item><Link to="/about">About</Link></Item>
+        <Item><Link to="/login">Log in</Link></Item>
+        <Item><Link to="/restaurants">Restaurants</Link></Item>
+        <Item><Link to="/xxx">멸망의 길</Link></Item>
+      </List>
+      <SlideContainer />
+    </Container>
   );
 }
