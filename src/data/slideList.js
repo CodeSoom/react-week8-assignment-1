4f1e78e6c8f61410bfbf804876dcad0a50e1bef0
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:17:26 +0900

    메타 데이터 sildeList 생성
    코드에는 영향을 주지 않지만, 기능에 영향을 주는 메타 데이터로 정리하여 코드를 리팩토링하였습니다

---
+++ src/data/slideList.js
@@ -0,0 +1,34 @@
+const slideList = [
+  {
+    id: 1,
+    list: [
+      { id: 1, src: 'slide-food-1' },
+      { id: 2, src: 'slide-food-2' },
+      { id: 3, src: 'slide-food-3' },
+      { id: 4, src: 'slide-food-4' },
+      { id: 5, src: 'slide-food-1' },
+    ],
+  },
+  {
+    id: 2,
+    list: [
+      { id: 1, src: 'slide-food-3' },
+      { id: 2, src: 'slide-food-4' },
+      { id: 3, src: 'slide-food-5' },
+      { id: 4, src: 'slide-food-6' },
+      { id: 5, src: 'slide-food-3' },
+    ],
+  },
+  {
+    id: 3,
+    list: [
+      { id: 1, src: 'slide-food-6' },
+      { id: 2, src: 'slide-food-7' },
+      { id: 3, src: 'slide-food-8' },
+      { id: 4, src: 'slide-food-9' },
+      { id: 5, src: 'slide-food-6' },
+    ],
+  },
+];
+
+export default slideList;
