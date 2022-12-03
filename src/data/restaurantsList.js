c1eff0d389317ba29f98dfd2928259a0cf98dfd8
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:16:24 +0900

    메타 데이터 restaurantsList 생성
    코드에는 영향을 주지 않지만, 기능에 영향을 주는 메타 데이터로 정리하여 코드를 리팩토링하였습니다

---
+++ src/data/restaurantsList.js
@@ -0,0 +1,12 @@
+const restaurantsList = [
+  '../../img/restaurant/restaurant_1.png',
+  '../../img/restaurant/restaurant_2.png',
+  '../../img/restaurant/restaurant_3.png',
+  '../../img/restaurant/restaurant_4.png',
+  '../../img/restaurant/restaurant_5.png',
+  '../../img/restaurant/restaurant_6.png',
+  '../../img/restaurant/restaurant_7.png',
+  '../../img/restaurant/restaurant_8.png',
+];
+
+export default restaurantsList;
