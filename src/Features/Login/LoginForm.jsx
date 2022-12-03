20875e3e92c773962ecbe0327ee8ee779322eec7
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:22:12 +0900

    LoginForm 컴포넌트 스타일링
    input을 활용하여 전체적인 스타일링을  하였습니다

--- src/Features/Login/LoginForm.jsx
+++ src/Features/Login/LoginForm.jsx
@@ -1,5 +1,72 @@
 import { memo } from 'react';
 
+import styled from '@emotion/styled';
+
+const UserBox = styled.div`
+  width: 100%;
+  height: 45px;
+  margin: .5rem 0;
+  border: 1px solid #4d4d4d;
+  padding-left: 5rem;
+  box-sizing: border-box;
+  position: relative;
+
+  &::before {
+    position: absolute;
+    display: block;
+    content: '';
+    background: url('../../img/icon/user-icon.png');
+    background-size: cover;
+    width: 24px;
+    height: 24px;
+    top: 50%;
+    left: 10px;
+    transform: translateY(-50%);
+  }
+`;
+
+const PasswordBox = styled.div`
+  width: 100%;
+  height: 45px;
+  margin: .5rem 0;
+  border: 1px solid #4d4d4d;
+  padding-left: 5rem;
+  box-sizing: border-box;
+  position: relative;
+
+  &::before {
+    position: absolute;
+    display: block;
+    content: '';
+    background: url('../../img/icon/lock-icon.png');
+    background-size: cover;
+    width: 24px;
+    height: 24px;
+    top: 50%;
+    left: 10px;
+    transform: translateY(-50%);
+  }
+`;
+
+const Label = styled.label`
+  display: none;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 45px;
+  border: none; 
+  background: transparent;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 45px;
+  margin-top: 10rem;
+  color : #ffffff;
+  background: #DB0816;
+`;
+
 const LoginForm = memo(({ fields, onChange, onSubmit }) => {
   const { email, password } = fields;
 
@@ -10,36 +77,38 @@ const LoginForm = memo(({ fields, onChange, onSubmit }) => {
 
   return (
     <>
-      <div>
-        <label htmlFor="login-email">
+      <UserBox>
+        <Label htmlFor="login-email">
           E-mail
-        </label>
-        <input
+        </Label>
+        <Input
           type="email"
           id="login-email"
           name="email"
           value={email}
           onChange={handleChange}
+          placeholder="아이디"
         />
-      </div>
-      <div>
-        <label htmlFor="login-password">
+      </UserBox>
+      <PasswordBox>
+        <Label htmlFor="login-password">
           Password
-        </label>
-        <input
+        </Label>
+        <Input
           type="password"
           id="login-password"
           name="password"
           value={password}
           onChange={handleChange}
+          placeholder="비밀번호"
         />
-      </div>
-      <button
+      </PasswordBox>
+      <Button
         type="button"
         onClick={onSubmit}
       >
         Log In
-      </button>
+      </Button>
     </>
   );
 });
