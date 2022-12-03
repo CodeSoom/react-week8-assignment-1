d938a65c8d55bfacc0bc8a1f04d86e8ad5a28a28
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-03 21:21:34 +0900

    LoginPage 컴포넌트 스타일링
    emotion을 활용하여 로그인 페이지를 스타일링하였습니다

--- src/Features/Login/LoginPage.jsx
+++ src/Features/Login/LoginPage.jsx
@@ -1,10 +1,54 @@
+import styled from '@emotion/styled';
+
 import LoginFormContainer from './LoginFormContainer';
+
+const Background = styled.div`
+  background: url(../../../img/main-bg.png);
+  width: 100%;
+  height: 89vh;
+  background-size: cover;
+`;
+
+const LoginBox = styled.div`
+  background: #ffffff;
+  width: 500px;
+  height: 500px;
+  padding: 5rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 10px;
+  -webkit-box-shadow: 5px 5px 8px 0px #CBCBCB; 
+  box-shadow: 5px 5px 8px 0px #CBCBCB;  
+
+  & h2 {
+    font-size: 5rem;
+    font-weight: 800;
+    position: relative;
+    margin-bottom: 10rem;
+
+    &::after {
+      display: block;
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 170px;
+      width: 15px;
+      height: 15px;
+      background-color: #DB0816;
+      border-radius: 50px;
+    }
+  }
+`;
 
 export default function LoginPage() {
   return (
-    <div>
-      <h2>Log In</h2>
-      <LoginFormContainer />
-    </div>
+    <Background>
+      <LoginBox>
+        <h2>Log In</h2>
+        <LoginFormContainer />
+      </LoginBox>
+    </Background>
   );
 }
