82615acc898d417da1c2b1c0cb2acea0e0437324
sejineeee <sejinee0918@gmail.com>
2022-12-03 21:28:09 +0900

    feat: slideList 배열 생성
    slide img의 src와 alt를 담은 slideList 배열 생성하여 export

---
+++ src/components/Slide/slide_img.js
@@ -0,0 +1,8 @@
+export const slideList = [
+  { id: 1, src: '/img/slide-img.jpg', alt: '한식 사진' },
+  { id: 2, src: '/img/slide-img2.jpg', alt: '음료 사진' },
+  { id: 3, src: '/img/slide-img3.jpg', alt: '빵 사진' },
+  { id: 4, src: '/img/slide-img4.jpg', alt: '플레이터 사진' },
+  { id: 5, src: '/img/slide-img5.jpg', alt: '레스토랑 사진' },
+  { id: 6, src: '/img/slide-img6.jpg', alt: '분식 사진' },
+];
