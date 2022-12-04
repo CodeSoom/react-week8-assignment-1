7f900e545973df562b7e266123f306b612f737f5
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:45:52 +0900

    Reviews.jsx style작업, 평점 숫자에서 별모양으로 변경

--- src/Features/Review/Reviews.jsx
+++ src/Features/Review/Reviews.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  background-color: #fff;
+  padding: 2rem 0 2rem 3rem;
+  border: 2px solid #797979;
+  border-radius: 42px;
+  list-style:none;
+  & li{
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid #ddd;
+  }
+`;
+
 function Reviews({ reviews }) {
   if (!reviews || !reviews.length) {
     return null;
@@ -7,23 +22,30 @@ function Reviews({ reviews }) {
 
   const sortedReviews = [...reviews].sort((a, b) => b.id - a.id);
 
+  function changeNumberToStar(number) {
+    if (number < 1) return '⭐️'.repeat(1);
+
+    if (number > 5) return '⭐️'.repeat(5);
+
+    return '⭐️'.repeat(number);
+  }
+
   return (
-    <ul>
+    <Wrapper>
       {sortedReviews.map((review) => (
         <li key={review.id}>
           <div>
             {review.name}
           </div>
           <div>
-            {review.score}
-            점
+            {changeNumberToStar(review.score)}
           </div>
           <div>
             {review.description}
           </div>
         </li>
       ))}
-    </ul>
+    </Wrapper>
   );
 }
 
