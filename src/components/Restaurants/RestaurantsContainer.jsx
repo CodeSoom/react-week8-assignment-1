c1efaddfe9438825ce6ab54b29256fd811c1e823
sejineeee <sejinee0918@gmail.com>
2022-12-05 03:04:32 +0900

    restaurants 스타일 구현

--- src/components/Restaurants/RestaurantsContainer.jsx
+++ src/components/Restaurants/RestaurantsContainer.jsx
@@ -1,6 +1,38 @@
 import { useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import { get } from '../../utils';
+
+const List = styled.ul({
+  borderTop: '1px solid #cccccc',
+  padding: '8px 16px',
+  display: 'flex',
+});
+
+const Item = styled.li({
+  width: '180px',
+  height: '180px',
+  backgroundColor: '#A30F0F',
+  border: '1px solid #A30F0F',
+  borderRadius: '50%',
+  margin: '12px',
+  '& a': {
+    display: 'block',
+    fontSize: '1.4rem',
+    fontFamily: 'Noto Sans KR',
+    color: '#ffffff',
+    padding: '30px',
+    textAlign: 'center',
+    transform: 'translateY(38px)',
+  },
+  '&:hover': {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    backgroundColor: '#E6A721',
+    border: '1px solid #E6A721',
+  },
+});
 
 export default function RestaurantsContainer({ onClickRestaurant }) {
   const restaurants = useSelector(get({ sliceName: 'restaurantSlice', key: 'restaurants' }));
@@ -13,14 +45,14 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
   }
 
   return (
-    <ul>
+    <List>
       {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
+        <Item key={restaurant.id}>
           <a href="/restaurants/1" onClick={handleClick(restaurant)}>
             {restaurant.name}
           </a>
-        </li>
+        </Item>
       ))}
-    </ul>
+    </List>
   );
 }
