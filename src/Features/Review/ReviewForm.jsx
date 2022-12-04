f2f8a736678aee8704cf0ec008331a0100993c37
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:46:51 +0900

    ReviewForm.jsx style작업

--- src/Features/Review/ReviewForm.jsx
+++ src/Features/Review/ReviewForm.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 import TextField from './TextField';
+
+const Wrapper = styled.div`
+  padding: 2rem;
+  background: #cacaca;
+  margin: 2rem 0;
+  border: 2px solid #f58410;
+  border-radius: 33px;
+  & div>input{
+    margin-left: 2rem;
+    margin-bottom: 1rem;
+    font-size: 2rem;
+  }
+  &>button{
+    background: #f58410;
+    border-radius: 10px;
+    padding: 0.8rem 2rem;
+    color: #fff;
+    margin-top: 2rem;
+    &:hover{
+      opacity:.7;
+    }
+  }
+`;
 
 function ReviewForm({ fields, onChange, onSubmit }) {
   const { score, description } = fields;
 
   return (
-    <>
+    <Wrapper>
       <TextField
         label="평점"
         name="score"
@@ -26,7 +51,7 @@ function ReviewForm({ fields, onChange, onSubmit }) {
       >
         리뷰 남기기
       </button>
-    </>
+    </Wrapper>
   );
 }
 
