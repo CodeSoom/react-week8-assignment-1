63805ba9aeb1921d46cc783226be4b6077bd5df5
sejineeee <sejinee0918@gmail.com>
2022-12-05 03:02:30 +0900

    categories, regions 스타일 구현

--- src/components/Categories/CategoriesContainer.jsx
+++ src/components/Categories/CategoriesContainer.jsx
@@ -1,11 +1,54 @@
 import { useDispatch, useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+
 import {
   selectCategory,
   loadRestaurants,
 } from '../../slice/restaurantSlice';
 
 import { get } from '../../utils';
+
+const List = styled.ul({
+  width: '100%',
+  margin: '14px auto',
+  display: 'flex',
+  flexWrap: 'wrap',
+});
+
+const Text = styled.p({
+  fontFamily: 'Noto Sans KR',
+  fontSize: '1.4rem',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  lineHeight: '1.8',
+  '&:after': {
+    content: '""',
+    display: 'block',
+    backgroundColor: '#A30F0F',
+    width: '67px',
+    height: '2px',
+  },
+});
+
+const Item = styled.li({
+  margin: '0.5rem 1rem',
+  textAlign: 'center',
+});
+
+const Button = styled.button(({ active }) => ({
+  width: '150px',
+  padding: '12px 0',
+  backgroundColor: active ? '#A30F0F' : 'transparent',
+  color: active ? '#ffffff' : '#000000',
+  fontSize: '18px',
+  fontFamily: 'Noto Sans KR',
+  borderRadius: '5px',
+  border: '1px solid #808080',
+}));
 
 export default function CategoriesContainer() {
   const dispatch = useDispatch();
@@ -19,12 +62,18 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <ul>
+    <List>
+      <Text>
+        <FontAwesomeIcon icon={faUtensils} />
+        {' '}
+        종류
+      </Text>
       {categories.map((category) => (
-        <li key={category.id}>
-          <button
+        <Item key={category.id}>
+          <Button
             type="button"
             onClick={() => handleClick(category.id)}
+            active={selectedCategory && category.id === selectedCategory.id}
           >
             {category.name}
             {selectedCategory ? (
@@ -32,9 +81,9 @@ export default function CategoriesContainer() {
                 {category.id === selectedCategory.id ? '(V)' : null}
               </>
             ) : null}
-          </button>
-        </li>
+          </Button>
+        </Item>
       ))}
-    </ul>
+    </List>
   );
 }
--- src/components/Regions/RegionsContainer.jsx
+++ src/components/Regions/RegionsContainer.jsx
@@ -1,11 +1,57 @@
 import { useDispatch, useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
+
 import {
   selectRegion,
   loadRestaurants,
 } from '../../slice/restaurantSlice';
 
 import { get } from '../../utils';
+
+const List = styled.ul({
+  width: '100%',
+  margin: '14px auto',
+  display: 'flex',
+  flexWrap: 'wrap',
+  borderBottom: '1px solid #cccccc',
+  paddingBottom: '1.2rem',
+  marginBottom: '2rem',
+});
+
+const Text = styled.p({
+  fontFamily: 'Noto Sans KR',
+  fontSize: '1.4rem',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  lineHeight: '1.8',
+  '&:after': {
+    content: '""',
+    display: 'block',
+    backgroundColor: '#A30F0F',
+    width: '67px',
+    height: '2px',
+  },
+});
+
+const Item = styled.li({
+  margin: '0.5rem 1rem',
+  textAlign: 'center',
+});
+
+const Button = styled.button(({ active }) => ({
+  width: '150px',
+  padding: '12px 0',
+  backgroundColor: active ? '#A30F0F' : 'transparent',
+  color: active ? '#ffffff' : '#000000',
+  fontSize: '18px',
+  fontFamily: 'Noto Sans KR',
+  borderRadius: '5px',
+  border: '1px solid #808080',
+}));
 
 export default function RegionsContainer() {
   const dispatch = useDispatch();
@@ -19,12 +65,18 @@ export default function RegionsContainer() {
   }
 
   return (
-    <ul>
+    <List>
+      <Text>
+        <FontAwesomeIcon icon={faMapLocationDot} />
+        {' '}
+        지역
+      </Text>
       {regions.map((region) => (
-        <li key={region.id}>
-          <button
+        <Item key={region.id}>
+          <Button
             type="button"
             onClick={() => handleClick(region.id)}
+            active={selectedRegion && region.id === selectedRegion.id}
           >
             {region.name}
             {selectedRegion ? (
@@ -32,9 +84,9 @@ export default function RegionsContainer() {
                 {region.id === selectedRegion.id ? '(V)' : null}
               </>
             ) : null}
-          </button>
-        </li>
+          </Button>
+        </Item>
       ))}
-    </ul>
+    </List>
   );
 }
