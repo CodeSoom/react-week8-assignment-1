c843e9d0b88f2d03676cccbbfbb1ff893536ea6a
sejineeee <sejinee0918@gmail.com>
2022-12-05 02:22:20 +0900

    logout form 스타일

--- src/components/LogoutForm/LogoutForm.jsx
+++ src/components/LogoutForm/LogoutForm.jsx
@@ -3,10 +3,16 @@ import styled from '@emotion/styled';
 const Button = styled.button({
   display: 'block',
   width: '180px',
-  padding: '8px 16px',
+  padding: '12px 16px',
   cursor: 'pointer',
-  fontSize: '16px',
+  fontSize: '1rem',
+  fontWeight: 'bold',
+  fontFamily: 'Roboto',
   margin: '24px auto',
+  backgroundColor: '#A30F0F',
+  borderRadius: '5px',
+  color: '#ffffff',
+  border: '1px solid #A30F0F',
 });
 
 export default function LogoutForm({ onClick }) {
