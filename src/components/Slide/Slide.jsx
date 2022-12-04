1c6f117058859fdd0a6c8eb8a19f7835248e5899
sejineeee <sejinee0918@gmail.com>
2022-12-05 03:58:19 +0900

    slide 스타일

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,12 +1,15 @@
 import styled from '@emotion/styled';
 
-const SlideItem = styled.img({
-  margin: '0 8px',
-  width: '1000px',
-});
+const SlideItem = styled.img(({ count }) => ({
+  width: '1200px',
+  objectFit: 'cover',
+  flexShrink: '0',
+  transform: `translateX(${-1200 * count}px)`,
+  transition: 'all 1s ease-in',
+}));
 
-export default function Slide({ src, alt }) {
+export default function Slide({ src, alt, count }) {
   return (
-    <SlideItem src={src} alt={alt} />
+    <SlideItem src={src} alt={alt} count={count} />
   );
 }
