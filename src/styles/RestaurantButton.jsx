a8f79d965c1d309995e06698ba1a0924f4e01db5
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:29:59 +0900

    styles폴더에 파일 생성 styles이 중복되는 코드를 파일로 만들어 공용으로 사용할 수 있도록 함

---
+++ src/styles/RestaurantButton.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const RestaurantButton = styled.button`
+  border-radius: 18px;
+  padding: 10px 23px;
+  font-size: 3rem;
+  background-color: #fff;
+  color: #FF5F00;
+  margin: 5px;
+  font-weight: 700;
+    &:hover{
+      border-radius: 18px;
+      opacity:0.8;
+    }
+`;
+
+export default RestaurantButton;
