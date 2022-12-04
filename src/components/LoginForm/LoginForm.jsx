1700b5fdaae962fe473f4cec4aa9f9a046fadf72
sejineeee <sejinee0918@gmail.com>
2022-12-04 12:31:31 +0900

    loginForm 스타일 구현
    label, input, button 스타일 적용

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,33 @@
+import styled from '@emotion/styled';
+
 import { memo } from 'react';
+
+const Container = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '400px',
+  margin: '16px auto',
+});
+
+const Label = styled.label({
+  fontSize: '20px',
+  fontFamily: 'Roboto',
+});
+
+const Input = styled.input({
+  width: '70%',
+  padding: '4px',
+});
+
+const Button = styled.button({
+  display: 'block',
+  width: '180px',
+  padding: '8px 16px',
+  cursor: 'pointer',
+  fontSize: '16px',
+  margin: '24px auto',
+});
 
 const LoginForm = memo(({ fields, onChange, onSubmit }) => {
   const { email, password } = fields;
@@ -10,36 +39,36 @@ const LoginForm = memo(({ fields, onChange, onSubmit }) => {
 
   return (
     <>
-      <div>
-        <label htmlFor="login-email">
+      <Container>
+        <Label htmlFor="login-email">
           E-mail
-        </label>
-        <input
+        </Label>
+        <Input
           type="email"
           id="login-email"
           name="email"
           value={email}
           onChange={handleChange}
         />
-      </div>
-      <div>
-        <label htmlFor="login-password">
+      </Container>
+      <Container>
+        <Label htmlFor="login-password">
           Password
-        </label>
-        <input
+        </Label>
+        <Input
           type="password"
           id="login-password"
           name="password"
           value={password}
           onChange={handleChange}
         />
-      </div>
-      <button
+      </Container>
+      <Button
         type="button"
         onClick={onSubmit}
       >
         Log In
-      </button>
+      </Button>
     </>
   );
 });
