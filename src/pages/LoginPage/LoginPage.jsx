0861091125d3e93334e413e9185b701642da9ac2
sejineeee <sejinee0918@gmail.com>
2022-12-05 02:21:51 +0900

    login page 스타일

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,17 +4,22 @@ import LoginFormContainer from '../../components/LoginForm/LoginFormContainer';
 
 const Container = styled.div({
   width: '600px',
-  height: '500px',
+  height: '600px',
   margin: '16px auto',
-  border: '1px solid #cccccc',
   padding: '8px 16px',
+  position: 'relative',
+  border: '1px solid #cccccc',
+  boxShadow: '2px 2px 3px rgb(163, 15, 15, .3)',
 });
 
 const Title = styled.h2({
   fontFamily: 'Roboto',
   fontSize: '32px',
   textAlign: 'center',
+  padding: '24px',
+  color: '#000000',
   marginBottom: '32px',
+  borderBottom: '1px solid #cccccc',
 });
 
 export default function LoginPage() {
