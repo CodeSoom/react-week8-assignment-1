687ff454c52c4085e03fee937daf5aa0cadcbe9c
yxxnhx <tjdbsgk514@hanmail.net>
2022-12-04 13:52:51 +0900

    useSlide에서 불필요한 boolean 값 제거
    true와 false로 확인할 필요 없이 elementLength로 값을 나누어 무한반복될 수 있도록 코드를 수정하였습니다

--- src/services/useSlide.js
+++ src/services/useSlide.js
@@ -1,29 +1,19 @@
 import { useState, useEffect } from 'react';
 
 const useSlide = (elementLength) => {
-  const [boolean, setBoolean] = useState(false);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const timer = setInterval(
-      () => {
-        if (count < elementLength - 1) {
-          setBoolean(false);
-          setCount((prev) => prev + 1);
-        } else {
-          setBoolean(true);
-          setCount(0);
-        }
-      },
-      boolean ? 0 : 2500,
-    );
+    const timer = setInterval(() => {
+      setCount((prev) => (prev + 1) % elementLength);
+    }, 2500);
 
     return () => {
       clearInterval(timer);
     };
-  }, [count]);
+  }, []);
 
-  return [count, boolean];
+  return count;
 };
 
 export default useSlide;
