499ea0f87e965290b5ffaea6cfc9cbc3f7b2ecce
sejineeee <sejinee0918@gmail.com>
2022-12-05 03:58:01 +0900

    slide 구현

--- src/components/Slide/SlideContainer.jsx
+++ src/components/Slide/SlideContainer.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import styled from '@emotion/styled';
 
 import Slide from './Slide';
@@ -6,17 +8,31 @@ import { slideList } from './slide_img';
 
 const Container = styled.div({
   display: 'flex',
+  width: '1200px',
   height: '600px',
   margin: '16px 0 0',
-  position: 'relative',
 });
 
 export default function SlideContainer() {
+  const [count, setCount] = useState(0);
+  const slideListLength = slideList.length;
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount((prev) => (prev + 1) % slideListLength);
+    }, 3000);
+  }, []);
+
   return (
     <Container>
       {
         slideList.map((item) => (
-          <Slide key={item.id} src={item.src} alt={item.alt} />
+          <Slide
+            key={item.id}
+            src={item.src}
+            alt={item.alt}
+            count={count}
+          />
         ))
       }
     </Container>
