bc483f5af51ffd5fb6136add5c10fcdfa045ad7c
sejineeee <sejinee0918@gmail.com>
2022-12-04 12:39:23 +0900

    logout button 스타일 구현
    Logout button 스타일적용

--- src/components/LogoutForm/LogoutForm.jsx
+++ src/components/LogoutForm/LogoutForm.jsx
@@ -1,12 +1,23 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button({
+  display: 'block',
+  width: '180px',
+  padding: '8px 16px',
+  cursor: 'pointer',
+  fontSize: '16px',
+  margin: '24px auto',
+});
+
 export default function LogoutForm({ onClick }) {
   return (
     <>
-      <button
+      <Button
         type="button"
         onClick={onClick}
       >
         Log out
-      </button>
+      </Button>
     </>
   );
 }
