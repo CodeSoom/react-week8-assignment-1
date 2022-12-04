cf686d6c1e1c05ed9f2b8aaf9ad92e1225146941
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:49:43 +0900

    LogoutForm.jsx style작업

--- src/Features/Login/LogoutForm.jsx
+++ src/Features/Login/LogoutForm.jsx
@@ -1,12 +1,26 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  width:100%;
+  padding:1rem 3rem;
+  background-color:#FF5F00;
+  color:#fff;
+  margin-top: 50px;
+  border-radius: 10px;
+  &:hover{
+    opacity:.7;
+  }
+`;
+
 export default function LogoutForm({ onClick }) {
   return (
     <>
-      <button
+      <Button
         type="button"
         onClick={onClick}
       >
         Log out
-      </button>
+      </Button>
     </>
   );
 }
