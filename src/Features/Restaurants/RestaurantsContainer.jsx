f9e8b3c38dbfb1d6ba046477535f76b24b410385
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:43:29 +0900

    RestaurantsContainer.jsx style작업

--- src/Features/Restaurants/RestaurantsContainer.jsx
+++ src/Features/Restaurants/RestaurantsContainer.jsx
@@ -1,6 +1,30 @@
 import { useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import { get } from '../../utils';
+
+const RestaurantsList = styled.ul`
+  margin-top:8rem;
+`;
+
+const Restaurant = styled.li`
+  display: inline-block;
+  border-radius: 10px;
+  padding: 10px 23px;
+  font-size: 3rem;
+  background-color: #fff;
+  color: #283c50;
+  margin: 5px;
+  font-weight: 700;
+`;
+
+const Title = styled.h2`
+  font-size: 2.2rem;
+  font-weight: 500;
+  color: #c7c7c7;
+  margin: 3rem 0 0.7rem 0;
+`;
 
 export default function RestaurantsContainer({ onClickRestaurant }) {
   const restaurants = useSelector(get('restaurants'));
@@ -13,14 +37,17 @@ export default function RestaurantsContainer({ onClickRestaurant }) {
   }
 
   return (
-    <ul>
+    <RestaurantsList>
+      <Title>
+        음식점을 누르면 리뷰와 평가를 볼 수 있어요.
+      </Title>
       {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
+        <Restaurant key={restaurant.id}>
           <a href="/restaurants/1" onClick={handleClick(restaurant)}>
             {restaurant.name}
           </a>
-        </li>
+        </Restaurant>
       ))}
-    </ul>
+    </RestaurantsList>
   );
 }
