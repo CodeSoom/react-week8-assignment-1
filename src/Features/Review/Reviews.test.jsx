5178733b8ca401cfac93c41e58f29564c36a7411
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:50:26 +0900

    Reviews.test.jsx 작성

--- src/Features/Review/Reviews.test.jsx
+++ src/Features/Review/Reviews.test.jsx
@@ -36,4 +36,45 @@ describe('Reviews', () => {
       });
     });
   });
+
+  context('범위 내의 평점일경우', () => {
+    it('평점에 맞게 별 개수가 랜더링된다', () => {
+      const reviews = [
+        {
+          id: 1, name: '테스터', description: '맛있어요', score: 1,
+        },
+        {
+          id: 2, name: '지나가는 사람', description: '맛없어요', score: 5,
+        },
+      ];
+
+      const { queryByText } = render((
+        <Reviews reviews={reviews} />
+      ));
+
+      reviews.forEach((review) => {
+        expect(queryByText('⭐️'.repeat(review.score))).not.toBeNull();
+      });
+    });
+  });
+
+  context('범위를 벗어났을 경우', () => {
+    it('설정한 개수에 맞게 별이 랜더링된다.', () => {
+      const reviews = [
+        {
+          id: 1, name: '테스터', description: '맛있어요', score: -1,
+        },
+        {
+          id: 2, name: '지나가는 사람', description: '맛없어요', score: 100,
+        },
+      ];
+
+      const { queryByText } = render((
+        <Reviews reviews={reviews} />
+      ));
+
+      expect(queryByText('⭐️')).not.toBeNull();
+      expect(queryByText('⭐️'.repeat(5))).not.toBeNull();
+    });
+  });
 });
