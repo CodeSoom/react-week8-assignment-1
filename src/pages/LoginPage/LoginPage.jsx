abdccedc1ddc8b8865c4246064b65bd046fac1b1
sejineeee <sejinee0918@gmail.com>
2022-12-04 12:29:59 +0900

    LoginPage 스타일 적용
    Login title 스타일, form과 title을 감싸고 있는 Container 스타일 구현

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,27 @@
+import styled from '@emotion/styled';
+
 import LoginFormContainer from '../../components/LoginForm/LoginFormContainer';
+
+const Container = styled.div({
+  width: '600px',
+  height: '500px',
+  margin: '16px auto',
+  border: '1px solid #cccccc',
+  padding: '8px 16px',
+});
+
+const Title = styled.h2({
+  fontFamily: 'Roboto',
+  fontSize: '32px',
+  textAlign: 'center',
+  marginBottom: '32px',
+});
 
 export default function LoginPage() {
   return (
-    <div>
-      <h2>Log In</h2>
+    <Container>
+      <Title>Log In</Title>
       <LoginFormContainer />
-    </div>
+    </Container>
   );
 }
