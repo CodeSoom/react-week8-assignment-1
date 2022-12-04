4d9b87d6c1882b0c40b8641ee2efb63db0fd993e
hyeji07 <zindex_ann@naver.com>
2022-12-04 18:27:38 +0900

    NavBar.jsx작성

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+const Nav = styled.ul({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const NavItem = styled.li({
+  padding: '0 4rem',
+  fontSize: '1.7rem',
+  fontWeight: '500',
+  '& a': {
+    display: 'block',
+  },
+});
+
+export default function NavBar() {
+  return (
+    <Nav>
+      <NavItem><Link to="/about">About</Link></NavItem>
+      <NavItem><Link to="/restaurants">Restaurants</Link></NavItem>
+      <NavItem><Link to="/login">Log in</Link></NavItem>
+    </Nav>
+  );
+}
